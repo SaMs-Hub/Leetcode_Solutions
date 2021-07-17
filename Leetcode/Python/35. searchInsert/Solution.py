731b35c4a7aced2de4aaba0ec4627a52ae665f11
Shafi Khan <76864645+SaMs-Hub@users.noreply.github.com>
2021-07-17 22:40:10 +0530

    Create Solution.py

---
+++ Leetcode/Python/35. searchInsert/Solution.py
@@ -0,0 +1,13 @@
+class Solution:
+    def searchInsert(self, nums: List[int], target: int) -> int:
+        l = 0
+        r = len(nums) - 1
+        while l <= r:
+            m = (l+r)//2
+            if m == r and target > nums[m]:
+                return m + 1
+            elif target > nums[m]:
+                l = m + 1
+            else:
+                r = m - 1
+        return m
