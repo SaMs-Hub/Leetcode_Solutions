ff335c2f14ec2b772aff2b474d79f15332616d2f
Shafi Khan <76864645+SaMs-Hub@users.noreply.github.com>
2021-07-19 21:59:30 +0530

    Create Solution.py

---
+++ Leetcode/Python/235. Lowest Common Ancestor/Solution.py
@@ -0,0 +1,15 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
+        s, b = sorted([p.val, q.val])
+        while not s <= root.val <= b:
+            # Keep searching since root is outside of [s, b].
+            root = root.left if s <= root.val else root.right
+        # s <= root.val <= b.
+        return root
