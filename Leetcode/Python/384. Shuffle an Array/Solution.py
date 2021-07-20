c95fa75b1b5e53f56ba4d8376f537fefd2d4d8f0
Shafi Khan <76864645+SaMs-Hub@users.noreply.github.com>
2021-07-20 23:09:00 +0530

    Create Solution.py

---
+++ Leetcode/Python/384. Shuffle an Array/Solution.py
@@ -0,0 +1,18 @@
+import random
+
+class Solution:
+
+    def __init__(self, nums: List[int]):
+        self.nums = nums
+
+    def reset(self) -> List[int]:
+        """
+        Resets the array to its original configuration and return it.
+        """
+        return self.nums
+
+    def shuffle(self) -> List[int]:
+        """
+        Returns a random shuffling of the array.
+        """
+        return random.sample(self.nums, len(self.nums))
