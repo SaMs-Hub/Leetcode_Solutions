19f34ba8a47f4f081f7ce16da873aad1445c50a3
Shafi Khan <76864645+SaMs-Hub@users.noreply.github.com>
2022-01-30 21:55:07 +0530

    Create 3. BinarySearch.cpp

---
+++ Topics/Arrays/3. BinarySearch.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int binarySearch(int arr[], int x, int n){
+    int s = 0;
+    int e = n - 1;
+
+    while (s <= e){
+        int mid = (s + e)/2;
+
+        if (arr[mid] == x){
+            return mid;
+        }else if (arr[mid] < x){
+            s = mid + 1;
+        }else{
+            e =  mid - 1;
+        }
+    }
+    return -1;
+}
+
+
+int main(){
+    int n, x;
+    cin >> n >> x;
+
+    int arr[] = {1, 2, 3, 4, 8};
+
+    cout << binarySearch(arr, x, n);
+}
