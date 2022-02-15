5e351f9a7681616328c91e19a2b219deeaef7573
Shafi Khan <76864645+SaMs-Hub@users.noreply.github.com>
2022-02-15 23:27:35 +0530

    Create 5. binarySearch.cpp

---
+++ Topics/Recursions/5. binarySearch.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int binarySearch(int arr[], int n, int key, int si, int ei){
+    if (si > ei){
+        return -1;
+    }
+    int mid = (si + ei)/2;
+    if (arr[mid] == key){
+        return mid;
+    }else if(arr[mid] < key){
+        return binarySearch(arr, n, key, mid + 1, ei);
+    }else{
+        return binarySearch(arr, n, key, si, mid - 1);
+    }
+}
+
+
+int main(){
+    int arr[] = {1, 2, 3, 4, 5};
+    int n = sizeof(arr)/sizeof(int);
+    int key = 4;
+
+    cout << binarySearch(arr, n, key, 0, n);
+   
+}
