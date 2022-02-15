1d372e2cde25eaee1945e4b97eeac97e7e005c3a
Shafi Khan <76864645+SaMs-Hub@users.noreply.github.com>
2022-02-15 23:36:53 +0530

    Create 10. isArraySorted.cpp

---
+++ Topics/Arrays/10. isArraySorted.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int isSorted(int *arr, int n){
+    for (int i = 0; i < n; i++){
+        if (arr[i] > arr[i + 1]){
+            return false;
+        }
+    }
+    return true;
+   
+
+
+}
+
+
+int main(){
+    int arr[] = {1, 2, 3, 4, 5};
+    int n = sizeof(arr)/sizeof(int);
+    
+
+    cout << isSorted(arr, n);
+   
+}
