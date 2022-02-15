71afa684c9e46ba38564b16ff7a12cb14cc4c978
Shafi Khan <76864645+SaMs-Hub@users.noreply.github.com>
2022-02-15 23:22:15 +0530

    Create 4. fibonnaciSeries.cpp

---
+++ Topics/Recursions/4. fibonnaciSeries.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int fib(int n){
+    // base case
+    if (n == 0 or n == 1){
+        return n;
+    }
+
+    // rec case
+    int f1 = fib(n - 1);
+    int f2 = fib(n - 2);
+    return f1 + f2;
+}
+
+int main(){
+    int n = 5;
+    cout << fib(n);
+}
