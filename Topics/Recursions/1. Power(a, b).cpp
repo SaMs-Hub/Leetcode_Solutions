e434c2a3f6a84998fa5dbf853a1ec19aeb025b88
Shafi Khan <76864645+SaMs-Hub@users.noreply.github.com>
2022-02-15 23:11:29 +0530

    Create  1. Power(a, b).cpp

---
+++ Topics/Recursions/1. Power(a, b).cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int power(int a, int b){
+    // base case
+    if (b == 0){
+        return 1;
+    }
+
+
+    // rec case
+    int smallOutput = power(a, b - 1);
+    return a * smallOutput;
+
+}
+
+int main(){
+    int a = 3;
+    int b = 2;
+
+    cout << power(a, b);
+}
