10342b2333006800c34a7ba24394048e129efc26
Shafi Khan <76864645+SaMs-Hub@users.noreply.github.com>
2022-03-15 14:59:43 +0530

    Update 15. arraySum.cpp

--- Topics/Recursions/15. arraySum.cpp
+++ Topics/Recursions/15. arraySum.cpp
@@ -1,3 +1,30 @@
+/// Appraoch 1 -- Basic
+#include <bits/stdc++.h>
+using namespace std;
+
+int arrSum(int arr[], int  n){
+     // base case
+     if (n == 0){
+          return 0;
+     }
+
+
+     // rec case
+     int smallerSum = arrSum(arr + 1, n - 1);
+
+     // return calc
+     return arr[0] + smallerSum;
+
+}
+
+int main(){
+     int arr[] = {1, 2, 3, 4, -5};
+     int n = sizeof(arr)/sizeof(int);
+
+     cout << arrSum(arr, n);;
+}
+
+/// Approach 2 -- using a variable startingIndex
 #include <bits/stdc++.h>
 using namespace std;
 
