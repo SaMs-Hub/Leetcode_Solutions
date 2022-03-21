9e25efa9503dc2f9421f66ae23e2ab42d24c507e
Shafi Khan <76864645+SaMs-Hub@users.noreply.github.com>
2022-03-21 14:46:06 +0530

    Update 1. printSubArraysWithSum0.cpp

--- Problems/1. printSubArraysWithSum0.cpp
+++ Problems/1. printSubArraysWithSum0.cpp
@@ -1,34 +1,53 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-void printSubZero(int arr[], int n){
-     // traverse over i
-     for (int i = 0; i < n; i++){
-          
-          int sum = 0;
-          for (int j = i; j < n; j++){
-               sum += arr[j];
+int printSubZero(int *arr, int n){
+    // make two vars to store start and end index of subArray
+    int start = 0;
+    int end = -1;
 
-               if (sum == 0){
-                    cout << "(" << i << "..." << j << ")" << endl;
-               }
-          }
-     }
 
-     // traverse over j
+    // traverse throught the array with j calls at i place
+    for (int i = 0; i < n;i++){
 
-     // get the sum
+        // make a var sum to store sum
+        int sum = 0;
+        for (int j = i; j < n; j++){
+            // keep adding sum at j iterations
+            sum += arr[j];
 
-     // print i, j if sum is 0
+            if (sum == 0){
+                start = i;
+                end = j;
+
+                for (int k = start; k <= end; k++){
+                    cout << arr[k] << ",";
+                }
+
+                cout << endl;
+            }
+
+            
+
+            
+        }
+    }
+
+    
+
+    // if sum is zero, print them
+
+
+
+    
 }
 
+int main()
+{
+    int arr[] = { 3, 4, -7, 3, 1, 3, 1, -4, -2, -2};
+    int n = sizeof(arr) / sizeof(int);
 
+    printSubZero(arr, n);
 
-int main(){
-     int arr[] =  { 3, 4, -7, 3, 1, 3, 1, -4, -2, -2 };
-     int n = sizeof(arr)/sizeof(int);
-
-     printSubZero(arr, n);
-
-     return 0;
+    return 0;
 }
