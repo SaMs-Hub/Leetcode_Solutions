04eb7179e9802b4fd073c6754a6b70fa958c0ce1
Shafi Khan <76864645+SaMs-Hub@users.noreply.github.com>
2022-03-23 22:58:05 +0530

    Create 3. FindLength.cpp

---
+++ Topics/Linked Lists/3. FindLength.cpp
@@ -0,0 +1,88 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Node
+{
+public:
+    int data;
+    Node *next;
+
+    Node(int data){
+        this->data = data;
+        next = NULL;
+    }
+};
+
+Node *takeInput()
+{
+    // make a static value, to run the input loop until
+    // it reaches value
+    int data;
+    cin >> data;
+
+    // assign null to head, tail before starting
+    Node *head = NULL;
+    Node *tail = NULL;
+
+    while (data != -1){
+    // make a dynamic object after every data
+     Node *NewNode = new Node(data);
+    // if head is null, store newNode in head,tail
+    if (head == NULL){
+        head = NewNode;
+        tail = NewNode;
+    }else{
+    // else call nextvalue in tail, and store newNode as tail
+        tail->next = NewNode;
+        tail = tail->next;
+
+    }
+
+
+        cin >> data;
+    }
+
+    // return head at last;
+    return head;
+
+
+}
+
+void print(Node *head)
+{
+    // make a temp variable to store head
+    Node *temp = head;
+
+    // run a loop until temp points to null
+    while (temp != NULL){
+    // console temp.data and assign nextValue to it
+        cout << temp->data << "->";
+        temp = temp->next;
+    }
+
+}
+
+int printLength(Node *head){
+    // create a var count to keep traversing count + 1
+    // until head becomes NULL
+    int count = 0;
+    while (head != NULL){
+        head = head->next;
+        count += 1;
+    }
+
+    cout << count;
+}
+
+
+int main()
+{
+    Node *head = takeInput();
+    
+    print(head);
+    cout << endl;
+    printLength(head);
+    return 0;
+
+
+}
