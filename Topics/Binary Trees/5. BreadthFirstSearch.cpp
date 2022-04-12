64c52cbd7019152dac486e2c390a3f3118aad6d8
Shafi Khan <76864645+SaMs-Hub@users.noreply.github.com>
2022-04-12 21:34:19 +0530

    Create 5. BreadthFirstSearch.cpp

---
+++ Topics/Binary Trees/5. BreadthFirstSearch.cpp
@@ -0,0 +1,66 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Node{
+	public:
+		int data;
+		Node* left;
+		Node* right;
+
+		Node(int data){
+			this->data = data;
+			left = NULL;
+			right = NULL;
+		}
+};
+
+Node* buildTree(){
+	// take user data
+	int data;
+	cin >> data;
+
+	// if data is -1, then count as NULL
+	if (data == -1){
+		return NULL;
+	}
+
+	// store the data in object 
+	Node* root = new Node(data);
+
+	// call same for left and right Nodes
+	root->left = buildTree();
+	root->right = buildTree();
+}
+
+
+void bfs(Node* root){
+	queue<Node*> q;
+	q.push(root);
+
+	while (!q.empty()){
+		Node* f = q.front();
+		cout << f->data << ' ';
+		q.pop();
+
+		if (f->left){
+			q.push(f->left);
+		}
+
+		if (f->right){
+			q.push(f->right);
+		}
+	}
+
+	return;
+}
+
+
+int main()
+{
+
+	Node* root = buildTree();
+
+	bfs(root);
+	
+	return 0;
+}
