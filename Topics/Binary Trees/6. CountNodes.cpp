09472d4cf27b97685e52eb79e2722c5acf8bc8d0
Shafi Khan <76864645+SaMs-Hub@users.noreply.github.com>
2022-04-13 21:44:26 +0530

    Create 6. CountNodes.cpp

---
+++ Topics/Binary Trees/6. CountNodes.cpp
@@ -0,0 +1,65 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Node
+{
+public:
+	int data;
+	Node *left;
+	Node *right;
+
+	Node(int data)
+	{
+		this->data = data;
+		left = NULL;
+		right = NULL;
+	}
+};
+
+Node *buildTree()
+{
+	int data;
+	cin >> data;
+
+	if (data == -1)
+	{
+		return NULL;
+	}
+
+	Node *root = new Node(data);
+
+	root->left = buildTree();
+	root->right = buildTree();
+
+	return root;
+}
+
+
+	if (root == NULL){
+		return;
+	}
+
+	cout << root->data << " ";
+
+	printNode(root->left);
+	printNode(root->right);
+}
+
+int countNodes(Node* root){
+	// base case
+	if (root == NULL){
+		return 0;
+	}
+
+	// return calc 1 for root
+	return 1 + countNodes(root->left) + countNodes(root->right);
+}
+int main()
+{
+	Node *root = buildTree();
+	cout << countNodes(root);
+
+
+	
+	return 0;
+}
