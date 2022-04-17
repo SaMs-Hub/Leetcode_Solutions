7160d7aeb41770adc2f7f2b6e48953915c5cc9a6
Shafi Khan <76864645+SaMs-Hub@users.noreply.github.com>
2022-04-17 20:43:40 +0530

    Update 2. ReverseStack.cpp

--- Topics/Stacks/2. ReverseStack.cpp
+++ Topics/Stacks/2. ReverseStack.cpp
@@ -1,3 +1,4 @@
+// Approach 1
 #include <bits/stdc++.h>
 using namespace std;
 
@@ -51,3 +52,39 @@ int main()
 	}
 	return 0;
 }
+
+
+
+// Approach 2 - Using Recusions
+void insertAtBottom(stack<int> &s, int x){
+	// base - if stack empty push x
+	// else take out top, store it  and pop
+	// call at smaller stock , push ata
+	if (s.empty()){
+		s.push(x);
+		return;
+	}
+
+	int data = s.top();
+	s.pop();
+
+	insertAtBottom(s, x);
+	s.push(data);
+}
+
+void reverseStack(stack<int> &s){
+	// base case, if stack is empty return;
+	// else take out the head and store it in a var and pop it
+	// call the func on smallerStack and add it to bottom
+	if(s.empty()){
+		return;
+	}
+
+	int x = s.top();
+	s.pop();
+
+	reverseStack(s);
+	insertAtBottom(s, x);
+
+}
+
