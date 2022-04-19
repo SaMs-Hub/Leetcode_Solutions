95be8940f798cfa6f0fb35c5513264588fef6001
Shafi Khan <76864645+SaMs-Hub@users.noreply.github.com>
2022-04-19 21:07:27 +0530

    Update 2.1 LargestSmallestNumber -STL.cpp

--- Topics/Arrays/2.1 LargestSmallestNumber -STL.cpp
+++ Topics/Arrays/2.1 LargestSmallestNumber -STL.cpp
@@ -6,7 +6,7 @@ int LargestSmallest(int arr[], int n){
     // using builtin sort function
     sort(arr, arr + n);
 
-    int largest = arr[n];
+    int largest = arr[n - 1];
     int smallest = arr[0];
 
     cout << largest << "," << smallest; 
