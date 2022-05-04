1b1baa105acd9b1a098b946370854533e809a88e
Shafi Khan <76864645+SaMs-Hub@users.noreply.github.com>
2022-05-04 21:25:39 +0530

    Update 2. MinStepsTo1.cpp

--- Topics/Dynamic Programming/2. MinStepsTo1.cpp
+++ Topics/Dynamic Programming/2. MinStepsTo1.cpp
@@ -37,3 +37,26 @@ int main()
     int dp[100] = {0};
    cout << minSteps(10, dp);
 }
+
+
+// App2 - BottomUp
+int minSteps(int n){
+      int dp[100] = {0};
+      for (int i = 2; i <= n; i++){
+          int op1, op2, op3;
+          op1 = op2 = op3 = INT_MAX;
+
+          if (n % 3 == 0){
+              op3 = dp[i/3];
+          }
+
+          if (n % 2 == 0){
+              op2 = dp[i/2];
+          }
+          op1 = dp[i - 1];
+
+          dp[i] = min(op1,min(op2, op3)) + 1;
+      }
+
+      return dp[n];
+}
