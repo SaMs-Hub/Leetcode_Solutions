ee2351f303106ccdeecddf7c07e4925af5236308
Shafi Khan <76864645+SaMs-Hub@users.noreply.github.com>
2022-05-04 16:49:50 +0530

    Update 1. FibonacciSeries.cpp

--- Topics/Dynamic Programming/1. FibonacciSeries.cpp
+++ Topics/Dynamic Programming/1. FibonacciSeries.cpp
@@ -1,3 +1,5 @@
+// Approach 1 - TopDown
+
 #include <bits/stdc++.h>
 using namespace std;
 
@@ -23,4 +25,37 @@ int main()
     int dp[100] ={0};
     cout << fib(6, dp);
     return 0;
+}
+
+
+// App2 - BottomUp
+
+int fib(int n){
+    int arr[100]= {0};
+
+    arr[1] = 1;
+    for (int i = 2; i <= n; i++){
+        arr[i] = arr[i - 1] + arr[i - 2];
+    }
+
+    return arr[n];
+}
+
+
+// App 3 - BottomUp Better Space
+int fib(int n){
+    if ((n == 0) or (n == 1)){
+        return n;
+    }
+
+    int a = 0;
+    int b = 1;
+    int c;
+    for (int i = 2; i <= n; i++){
+        c = a + b;
+        a = b;
+        b = c;
+    }
+
+    return c;
 }
