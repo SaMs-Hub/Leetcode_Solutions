7fa9029cfec2ca5f913c0ddd203183906c83cfbb
Shafi Khan <76864645+SaMs-Hub@users.noreply.github.com>
2022-05-07 00:04:57 +0530

    Update 6. inserstionSort.cpp

--- Topics/Arrays/6. inserstionSort.cpp
+++ Topics/Arrays/6. inserstionSort.cpp
@@ -1,16 +1,27 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-void insertionSort(int arr[], int n){
-    for (int i = 1; i <= n - 1; i++){
-        int e = arr[i];
-        int j = i - 1;
-        while (j >= 0 and arr[j] > e){
-            arr[j + 1] = arr[j];
-            j -= 1;
-        }
-        arr[j + 1] = e;
-    }
+void insertionSort(vector<int> &arr) {
+   // get the size of array
+        int n = arr.size();
+
+        // imagine the 1st element is sorted, and run a loop from 2nd to last element
+        for (int i = 1; i <= n - 1; i++)
+        {
+            // store currentElement(arr[i]) in a bucktert(e)
+            // and take j as i - 1
+            int bucket = arr[i];
+            int j = i - 1;
+            // run a while loop unital j >= 0 and jth element > bucket by decreasing j
+            while (j >= 0 and arr[j] > bucket)
+            {
+                // store jth element in j + 1
+                arr[j + 1] = arr[j];
+                j -= 1;
+            }
+              // put bucket element in j + 1
+        arr[j + 1] = bucket;
+		}
 }
 
 
