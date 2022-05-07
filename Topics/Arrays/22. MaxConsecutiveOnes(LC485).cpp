ed432259f7d820f01ae57a6f6acab4faaf0cbfb3
Shafi Khan <76864645+SaMs-Hub@users.noreply.github.com>
2022-05-07 17:08:03 +0530

    Create 22. MaxConsecutiveOnes(LC485).cpp

---
+++ Topics/Arrays/22. MaxConsecutiveOnes(LC485).cpp
@@ -0,0 +1,45 @@
+// Approach 1 - BruteForce using same array
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int getMax(vector<int> arr){
+    // get the size of arr and traverse throught it
+    int n = arr.size();
+    for (int i = 1; i < n; i++){
+    // if arr[i] is 1, set i as += i - 1 else as 0
+       arr[i]== 1 ? arr[i] += arr[i - 1] : arr[i] = 0;
+    }
+    // reutrn maxElement of arr
+    return *max_element(arr.begin(), arr.end());
+}
+int main()
+{
+    vector<int> arr{1, 1, 5, 1, 1, 2, 1};
+
+    cout << getMax(arr);
+}
+
+
+// App2 - Using vars
+int findMaxConsecutiveOnes(vector<int>& arr) {
+          int currCount = 0;
+    int maxCount = 0;
+
+    // get the size of arr and traverse throught it
+    int n = arr.size();
+    for (int i = 0; i < n; i++){
+        // if element is 1, increase currCount
+        if (arr[i] == 1){
+            currCount += 1;
+        }else{
+            // else get max of maxCount, currntCount & set currentCount as zero
+            maxCount = max(currCount, maxCount);
+            currCount = 0;
+        }
+    }
+
+    // get maxCount n return it
+    maxCount = max(currCount, maxCount);
+    return maxCount; 
+    }
