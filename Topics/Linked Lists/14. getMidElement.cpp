35013d85f4e0dc7c7443595aee6c9cb888080e20
Shafi Khan <76864645+SaMs-Hub@users.noreply.github.com>
2022-05-23 01:18:33 +0530

    Create 14. getMidElement.cpp

---
+++ Topics/Linked Lists/14. getMidElement.cpp
@@ -0,0 +1,31 @@
+
+int getMiddleElementOfLinkedList (ListNode* head) {
+    
+
+	// count the size of list
+    int count = 0;
+    ListNode* currentNode = head;
+    while (currentNode != NULL){
+        count += 1;
+        currentNode = currentNode->next;
+    }
+    
+	// if size is even, decrease by 1
+    int mid = count/2;
+    if (count % 2 == 0){
+        mid--;
+    }
+
+	
+	// iterate till mid and get back currentNodeData
+    currentNode = head;
+    int curr = 0;
+    while (curr != mid){
+        curr++;
+        currentNode = currentNode->next;
+    }
+
+    return currentNode->data;
+    
+
+}
