c15fda187fead2fc656a3d9e7ba04f89cb89a507
Shafi Khan <76864645+SaMs-Hub@users.noreply.github.com>
2022-05-23 01:02:37 +0530

    Create 13. RemoveOccurences.cpp

---
+++ Topics/Linked Lists/13. RemoveOccurences.cpp
@@ -0,0 +1,27 @@
+
+Node *removeOccurence(Node *head, int key)
+{
+    // base if headData is key point head to headNext
+    while (head != NULL && head->data == key)
+    {
+        head = head->next;
+    }
+    // make temp CurrentNode while its next != NULL
+
+    Node *currentNode = head;
+    while (currentNode != NULL && currentNode->next != NULL)
+    {
+        // if it data is key, point it to nextNext
+        if (currentNode->next->data == key)
+        {
+            currentNode->next = currentNode->next->next;
+        }
+        else
+        {
+            // else point to currNext
+            currentNode = currentNode->next;
+        }
+    }
+
+    return head;
+}
