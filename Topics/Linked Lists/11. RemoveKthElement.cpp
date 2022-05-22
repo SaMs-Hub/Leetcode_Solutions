8227384be8b738df713e1a28c6e62ebaaebb1f4a
Shafi Khan <76864645+SaMs-Hub@users.noreply.github.com>
2022-05-23 00:43:45 +0530

    Create 11. RemoveKthElement.cpp

---
+++ Topics/Linked Lists/11. RemoveKthElement.cpp
@@ -0,0 +1,25 @@
+ListNode* removekthElement (ListNode* head, int k) {
+     // make var to store currnt index and make a tempNode poiting to head
+    int index = 0;
+    ListNode* currentNode = head;
+	
+	// base case if k is 1 return headNext
+    if (k == 1){
+        return head->next;
+    }
+    
+    // while tempNode not null, keep iterating till k - 1, setting currentNode as currentNext
+    while (currentNode != NULL){
+        index += 1;
+        if (index == k - 1){
+            break;
+        }
+        currentNode = currentNode->next;
+    }
+    // poing currentNext as currNExtNext
+    currentNode->next = currentNode->next->next;
+
+
+    // return head;
+    return head;
+}
