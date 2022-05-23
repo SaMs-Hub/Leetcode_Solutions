966c0497c5a6aa965cade35efeb01fad84c15cd0
Shafi Khan <76864645+SaMs-Hub@users.noreply.github.com>
2022-05-23 23:22:41 +0530

    Create 1. SquareRoot(LC69).cpp

---
+++ Topics/Searching/1. SquareRoot(LC69).cpp
@@ -0,0 +1,46 @@
+// App1 - Naive
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int mySqrt(int x)
+
+{
+    // base case - if x is 0 or 1 return x
+    if (x == 0 || x == 1){
+        return x;
+    }
+    // iterate from 1 to n - 1, if i * i > nums, return n - 1
+    for (int i = 1; i < x; i++){
+        if (i * i > x){
+            return i - 1;
+        }
+    }
+
+}
+
+int main()
+{
+    cout << mySqrt(5);
+    return 0;
+}
+
+
+// App 2- iterative - BinarySearch
+ int mySqrt(int num) {
+    if (num == 0 || num == 1) {
+       return num;
+   }
+   long long low = 1, high = num, mid, ans = 1;
+   while (low <= high) {
+       mid = (high + low) / 2;
+       if (mid <= num / mid) {
+           low = mid + 1;
+           ans = mid;
+       } else {
+           high = mid - 1;
+       }
+   }
+   return ans;
+
+}
