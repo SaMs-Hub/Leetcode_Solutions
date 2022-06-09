40f656a7bcfd9efcb67ba401a1595a230a455d89
Shafi Khan <76864645+SaMs-Hub@users.noreply.github.com>
2022-06-09 14:50:08 +0530

    Create 13. Inordertraversal(LC94).cpp

---
+++ Topics/Binary Trees/13. Inordertraversal(LC94).cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    void helper(TreeNode* root, vector<int> &res){
+        if (root ==  NULL){
+            return;
+        }
+        
+        helper(root->left, res);
+        res.push_back(root->val);
+        helper(root->right, res);
+    }
+    vector<int> inorderTraversal(TreeNode* root) {
+        vector<int> res;
+        helper(root, res);
+        return res;
+        
+        
+    }
+};
