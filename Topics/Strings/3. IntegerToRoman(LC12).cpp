10d1265db8469e7a75346410c189f8662505a507
Shafi Khan <76864645+SaMs-Hub@users.noreply.github.com>
2022-06-16 23:00:08 +0530

    Update 3. IntegerToRoman(LC12).cpp

--- Topics/Strings/3. IntegerToRoman(LC12).cpp
+++ Topics/Strings/3. IntegerToRoman(LC12).cpp
@@ -36,3 +36,34 @@ string IntergerToRoman(int n)
 
     return thousands[n / 1000] + hundreds[(n % 1000) / 100] + tens[(n % 100) / 10] + ones[n % 10];
 }
+
+
+
+
+// Approach 3 - Using Maps
+string intToRoman(int n){
+    if (n == 0){
+        return "";
+    }
+
+    map<int, string> str {
+		{1,"I"},{4,"IV"},{5,"V"},{9,"IX"},{10,"X"},{40,"XL"}, {50,"L"},{90,"XC"},{400,"CD"},{500,"D"},{900,"CM"},{1000,"M"} 
+		};
+
+    string res = "";
+
+    while (n != 0){
+        map<int, string>::iterator p = str.upper_bound(n);
+        p--;
+        n -= p->first;
+        res += p->second;
+
+
+
+    }
+
+    return res;
+
+}
+
+
