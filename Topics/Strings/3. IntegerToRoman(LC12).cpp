182dc659a0388666b1ac97b7740caf0d1d8564e0
Shafi Khan <76864645+SaMs-Hub@users.noreply.github.com>
2022-06-16 14:42:06 +0530

    Update 3. IntegerToRoman(LC12).cpp

--- Topics/Strings/3. IntegerToRoman(LC12).cpp
+++ Topics/Strings/3. IntegerToRoman(LC12).cpp
@@ -1,3 +1,5 @@
+// Approach 1 - Using Loop 
+
 class Solution {
 public:
     string intToRoman(int n) {
@@ -20,3 +22,17 @@ public:
         
     }
 };
+
+
+
+// Approach 2 -
+
+string IntergerToRoman(int n)
+{
+    string ones[] = {"", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX"};
+    string tens[] = {"", "X", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC"};
+    string hundreds[] = {"", "C", "CC", "CCC", "CD", "D", "DC", "DCC", "DCCC", "CM"};
+    string thousands[] = {"", "M", "MM", "MMM"};
+
+    return thousands[n / 1000] + hundreds[(n % 1000) / 100] + tens[(n % 100) / 10] + ones[n % 10];
+}
