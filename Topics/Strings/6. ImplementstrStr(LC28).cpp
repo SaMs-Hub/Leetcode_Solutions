9bad7d9d651f5a05746364237549bc0fbfd376a6
Shafi Khan <76864645+SaMs-Hub@users.noreply.github.com>
2022-06-17 00:01:00 +0530

    Create 6. ImplementstrStr(LC28).cpp

---
+++ Topics/Strings/6. ImplementstrStr(LC28).cpp
@@ -0,0 +1,30 @@
+// App 1 - 
+
+class Solution {
+public:
+    int strStr(string s1, string s2) {
+        if (s1.size() < s2.size()){
+        return -1;
+    }
+
+    for (int i = 0; i <= s1.size() - s2.size(); i++){
+        int subs = 1;
+        for (int j = 0; j < s2.size(); j++){
+            cout << "values" << s1[i + j]  << ' ' << s2[j]  << endl;
+            if (s1[i + j] != s2[j]){
+
+            
+            subs = 0;
+            break;
+            }
+        }
+
+        if (subs == 1){
+            return i;
+        }
+    }
+
+    return -1;
+        
+    }
+};
