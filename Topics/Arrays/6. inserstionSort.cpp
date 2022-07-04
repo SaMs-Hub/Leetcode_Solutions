528e9ebb7e695f98046fd1f7b6451071b17b728e
Shafi Khan <76864645+SaMs-Hub@users.noreply.github.com>
2022-07-04 23:12:12 +0530

    Update 6. inserstionSort.cpp

--- Topics/Arrays/6. inserstionSort.cpp
+++ Topics/Arrays/6. inserstionSort.cpp
@@ -37,3 +37,35 @@ int main(){
         cout << arr[i] << ",";
     }
 }
+
+
+
+// Same using vectors
+#include <bits/stdc++.h>
+using namespace std;
+
+void insertionSort(vector<int> &arr){
+    int n = arr.size();
+    for (int i = 1; i < n; i++){
+        int currentElement = arr[i];
+        int position = i - 1;
+
+        while (position >= 0 and arr[position] > currentElement){
+            arr[position + 1] = arr[position];
+            position -= 1;
+        }
+
+        arr[position + 1] = currentElement;
+    }
+}
+
+
+int main()
+{
+   vector<int> arr{1, 8, 0, -2, 6};
+
+   insertionSort(arr);
+   for (auto x: arr){
+    cout << x << ',';
+   }
+}
