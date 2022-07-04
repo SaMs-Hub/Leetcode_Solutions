412204387db3dc27800f20c493d45b76c4f8f6bf
Shafi Khan <76864645+SaMs-Hub@users.noreply.github.com>
2022-07-04 23:58:01 +0530

    Update 24. MergeSortedArrays(LC88).cpp

--- Topics/Arrays/24. MergeSortedArrays(LC88).cpp
+++ Topics/Arrays/24. MergeSortedArrays(LC88).cpp
@@ -38,3 +38,21 @@ vector<int> mergeSort(vector<int> arr1, vector<int> arr2){
     return res;
 }
 
+// Approach 3
+ void merge(vector<int>& arr1, int m, vector<int>& arr2, int n) {
+
+    int i = m-1, j = n - 1, k = (m + n) - 1;
+
+    while (i >= 0 && j  >= 0){
+        if (arr1[i] > arr2[j]) arr1[k--] = arr1[i--];
+        else arr1[k--] = arr2[j--];
+    }
+
+
+    while (j  >= 0){
+        arr1[k--] = arr2[j--];
+    }
+    
+        
+    }
+
