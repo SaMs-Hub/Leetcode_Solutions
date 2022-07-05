0b51f95b771aa4407e151ef0d202a09e03ba0b87
Shafi Khan <76864645+SaMs-Hub@users.noreply.github.com>
2022-07-05 14:57:25 +0530

    Update 2. Anagram(LC.242).cpp

--- Topics/Strings/2. Anagram(LC.242).cpp
+++ Topics/Strings/2. Anagram(LC.242).cpp
@@ -24,3 +24,27 @@ int main()
     cout << isAnagram(A, B);
     return 0;
 }
+
+
+
+// Approach 2 - Using Maps
+class Solution {
+public:
+    bool isAnagram(string A, string B) {
+    if (A.size() != B.size()){
+        return false;
+    }
+
+    unordered_map<char, int> value;
+    for (auto c: A){
+        value[c]++;
+    }
+
+    for (auto c: B){
+        value[c]--;
+        if (value[c] < 0) return false;
+    }
+
+    return true;
+}
+};
