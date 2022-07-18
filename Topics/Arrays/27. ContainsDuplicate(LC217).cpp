fc745a6f81ba70684bf80ab5a772002cf75a7966
Shafi Khan <76864645+SaMs-Hub@users.noreply.github.com>
2022-07-18 14:46:03 +0530

    Update 27. ContainsDuplicate(LC217).cpp

--- Topics/Arrays/27. ContainsDuplicate(LC217).cpp
+++ Topics/Arrays/27. ContainsDuplicate(LC217).cpp
@@ -16,8 +16,24 @@ public:
 
 
 
-// App 2 - O(n)
+// App 2 - Using sets O(n)
 bool containsDuplicates(vector<int> arr){
     set<int> res(arr.begin(), arr.end());
     return res.size() != arr.size();
 }
+
+
+// App 3 - Using hashmaps
+bool containsDuplicates(vector<int> arr){
+    map<int, int> res;
+    for (auto x: arr){
+        res[x]++;
+    }
+
+    for (auto y: res){
+        if (y.second >= 2) return true;
+    }
+    return false;
+}
+    
+    
