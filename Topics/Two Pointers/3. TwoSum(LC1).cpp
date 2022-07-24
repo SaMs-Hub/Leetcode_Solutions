b73ab39cb15525949f2d1b3107b772181f244825
Shafi Khan <76864645+SaMs-Hub@users.noreply.github.com>
2022-07-24 14:51:03 +0530

    Update 3. TwoSum(LC1).cpp

--- Topics/Two Pointers/3. TwoSum(LC1).cpp
+++ Topics/Two Pointers/3. TwoSum(LC1).cpp
@@ -15,3 +15,29 @@ public:
 
 // App 2 - Two Pointers
 
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& arr, int target) {
+        int n = arr.size();
+    vector<pair<int, int>> res;
+
+    for (int i = 0; i < n; i++){
+        res.push_back({arr[i], i});
+    }
+    sort(res.begin(), res.end());
+    int start = 0;
+    int end = n - 1;
+    int id1 = 0;
+    int id2 = 0;
+    while (start < end){
+        if (res[start].first + res[end].first == target) {
+           id1 = res[start].second;
+           id2 = res[end].second;
+           break;
+        }
+        else if(res[start].first + res[end].first < target) start++;
+       else end--;
+    }
+    return {id1, id2};
+    }
+};
