c47faf5969edd8b0e6cb4f25632dee1f7f6cacf9
Shafi Khan <76864645+SaMs-Hub@users.noreply.github.com>
2022-07-28 00:24:09 +0530

    Create 5. maxVowels(LC1456).cpp

---
+++ Topics/Two Pointers/5. maxVowels(LC1456).cpp
@@ -0,0 +1,25 @@
+// App 1 - Naive
+class Solution {
+public:
+    int maxVowels(string str, int k) {
+         int len = str.length();
+    int result = 0;
+    for (int i = 0; i < len; i++){
+        for (int j = i; j < len; j++){
+            if (j - i + 1 == k){
+                int counter = 0;
+                for (int l = i; l <= j; l++){
+                    if (str[l] == 'a' || str[l] == 'e' || str[l] == 'i' || str[l] == 'o' || str[l] == 'u') {
+                    counter += 1;    
+                    }
+                }
+
+                result = max(counter, result);
+            }
+        }
+    }
+    return result;
+    }
+};
+
+// App 2 
