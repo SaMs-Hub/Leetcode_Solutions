1ade6bcfce93827743def09373380a2346077c50
Shafi Khan <76864645+SaMs-Hub@users.noreply.github.com>
2022-07-30 01:10:14 +0530

    Create 7. BalancedParanthesis(LC20).cpp

---
+++ Topics/Stacks/7. BalancedParanthesis(LC20).cpp
@@ -0,0 +1,21 @@
+
+bool isBalancedParentheses(string str) {
+    int n = str.length();
+    stack<char> build;
+    for (int i = 0; i < n; i++){
+      if(str[i] == '(' || str[i] == '[' || str[i] == '{') build.push(str[i]);
+      else if(str[i] == ')'){
+        if (i == 0 or build.empty() or build.top() != '(') return false;
+        build.pop();
+      }
+      else if(str[i] == ']'){
+        if (i == 0 or build.empty() or build.top() != '[') return false;
+        build.pop();
+      }
+      else if(str[i] == '}'){
+        if (i == 0 or build.empty() or build.top() != '{') return false;
+        build.pop();
+      }
+    }
+    return build.empty();
+}
