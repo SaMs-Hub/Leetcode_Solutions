2e1fd728a993656e74596b68467f8832cde0e25c
Shafi Khan <76864645+SaMs-Hub@users.noreply.github.com>
2022-08-02 00:26:07 +0530

    Update 3. FirstLast(LC34).cpp

--- Topics/Searching/3. FirstLast(LC34).cpp
+++ Topics/Searching/3. FirstLast(LC34).cpp
@@ -1,4 +1,4 @@
-// App 1 - Naive
+// App 1 - Naive - O(n)
 
 vector<int> searchRange(vector<int>& arr, int target) {
          int first =-1;
@@ -16,4 +16,40 @@ vector<int> searchRange(vector<int>& arr, int target) {
         return {first, last};
     }
 
-// App 2 - 
+// App 2 - BinarySearch O(logN)
+int findStart(vector<int> arr, int low, int high, int key){
+  int n = arr.size();
+
+ if (high >= low) {
+       int mid = (high + low) / 2;
+       if ((mid == 0 || key > arr[mid - 1]) && arr[mid] == key) {
+           return mid;
+       } else if (key > arr[mid]) {
+           return findStart (arr, mid + 1, high, key);
+       } else {
+           return findStart (arr, low, mid - 1, key);
+       }
+   }
+   return -1;
+}
+
+int findEnd(vector<int> arr, int low, int high, int key, int n){
+
+
+  if (high >= low) {
+       int mid = (high + low) / 2;
+       if ((mid == n - 1 || key < arr[mid + 1]) && arr[mid] == key) {
+           return mid;
+       } else if (key < arr[mid]) {
+           return findEnd (arr, low, mid - 1, key, n);
+       } else {
+           return findEnd (arr, mid + 1, high, key, n);
+       }
+   }
+  return -1;
+}
+
+vector<int> searchRange(vector<int> arr, int key){
+  int n = arr.size();
+  return {findStart(arr, 0, n - 1, key), findEnd(arr, 0, n - 1, key, n)};
+}
