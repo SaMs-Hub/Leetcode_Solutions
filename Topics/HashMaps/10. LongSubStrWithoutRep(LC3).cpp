0056fe076740611283a2667502499dd8582a009c
Shafi Khan <76864645+SaMs-Hub@users.noreply.github.com>
2022-08-10 00:44:14 +0530

    Create 10. LongSubStrWithoutRep(LC3).cpp

---
+++ Topics/HashMaps/10. LongSubStrWithoutRep(LC3).cpp
@@ -0,0 +1,40 @@
+// App 1 - Naive O(n)
+int lengthOfLongestSubstring(string str){
+    int n = str.size();
+    int ans = 0;
+
+    for (int i = 0; i < n; i++){
+        for (int j = i; j < n; j++){
+            int flag = 1;
+            map<int,int> res;
+            for (int k = i; k <= j; k++){
+                if (res[str[k] - 'a'] == 1){
+                    flag = 0;
+                    break;
+                }
+                res[str[k] - 'a'] = 1;
+            }
+
+            if (flag == 1) ans = max(ans, j - i + 1);
+        }
+    }
+    return ans;
+}
+
+// App 2 - Hashing O(n)
+class Solution {
+public:
+    int lengthOfLongestSubstring(string str) {
+      int n = str.size();
+      int maxSub = 0;
+      vector<int> res(26, -1);
+
+      int start = 0;
+      for (int i = 0; i < n; i++){
+          if (res[str[i] - 'a'] >= start) start = res[str[i] - 'a'] + 1;
+          res[str[i] - 'a'] = i;
+          maxSub = max(maxSub, i - start + 1);
+      }
+      return maxSub;
+      }
+};
