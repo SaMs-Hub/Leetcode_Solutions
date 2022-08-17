8965812d040f743b99507835c52672d4ced61536
Shafi Khan <76864645+SaMs-Hub@users.noreply.github.com>
2022-08-17 23:35:59 +0530

    Create 8. LongestPalindromeSubstring(LC5).cpp

---
+++ Topics/Strings/8. LongestPalindromeSubstring(LC5).cpp
@@ -0,0 +1,33 @@
+// Naive - O(n3)
+class Solution {
+public:
+    string longestPalindrome(string str) {
+         int n = str.length();
+    int index = 0;
+    int palindromeLength = 0;
+
+    for (int i = 0; i < n; i++){
+        for (int j = i; j < n; j++){
+            bool isPalindrome = true;
+            for (int k = i; k <= j; k++){
+                if (str[k] != str[j - k + i]) isPalindrome = false;
+            }
+
+            if (isPalindrome == true && j - i + 1 > palindromeLength){
+                index = i;
+                palindromeLength = j - i + 1;
+            }
+
+        }
+    }
+
+    string ans = "";
+    for (int i = index; i < index + palindromeLength; i++){
+        ans += str[i];
+    }
+    return ans;
+
+        
+    }
+};
+
