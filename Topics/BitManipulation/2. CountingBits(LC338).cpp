b8f93b91badf6963b9adc0ed2020060395897a4b
Shafi Khan <76864645+SaMs-Hub@users.noreply.github.com>
2022-08-19 00:14:54 +0530

    Create 2. CountingBits(LC338).cpp

---
+++ Topics/BitManipulation/2. CountingBits(LC338).cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    vector<int> countBits(int n) {
+        vector<int> res(n + 1);
+
+    for (int i = 1; i <= n; i++){
+        res[i] = res[i >> 1] + (i & 1);
+    }
+    return res;
+        
+    }
+};
