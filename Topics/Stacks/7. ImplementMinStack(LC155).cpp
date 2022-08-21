9a95e1b6a54e9cbe9de7fef3995ff6d6059dd28c
Shafi Khan <76864645+SaMs-Hub@users.noreply.github.com>
2022-08-21 22:40:10 +0530

    Create 7. ImplementMinStack(LC155).cpp

---
+++ Topics/Stacks/7. ImplementMinStack(LC155).cpp
@@ -0,0 +1,32 @@
+// App 1 - Naive
+class MinStack {
+public:
+    stack<int> stack1, stack2;
+    void push(int x) {
+		stack1.push(x);
+		if (stack2.empty() or x <= getMin()) stack2.push(x);
+		
+    }
+    void pop() {
+		if(stack1.top() == getMin()) stack2.pop();
+		stack1.pop();
+	    
+    }
+    int top() {
+		return stack1.top();
+	    
+    }
+    int getMin() {
+		return stack2.top();
+	    
+    }
+};
+
+/**
+ * Your MinStack object will be instantiated and called as such:
+ * MinStack* obj = new MinStack();
+ * obj->push(val);
+ * obj->pop();
+ * int param_3 = obj->top();
+ * int param_4 = obj->getMin();
+ */
