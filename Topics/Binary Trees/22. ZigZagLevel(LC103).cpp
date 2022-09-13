a701f231394804ff92c1431dd16e49188c772b15
Shafi Khan <76864645+SaMs-Hub@users.noreply.github.com>
2022-09-14 00:26:27 +0530

    Update 22. ZigZagLevel(LC103).cpp

--- Topics/Binary Trees/22. ZigZagLevel(LC103).cpp
+++ Topics/Binary Trees/22. ZigZagLevel(LC103).cpp
@@ -19,3 +19,54 @@ public:
     }
 };
 
+// App 2- Queue BFS O(n)
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    vector<vector<int>> zigzagLevelOrder(TreeNode* root) {
+        vector<vector<int>> res;
+        if (!root) return res;
+        queue<TreeNode*> q;
+        vector<int> row;
+        int level = 0;
+        q.push(root);
+        
+        while (!q.empty()){
+            int n = q.size();
+            for (int i = 0; i < n; i++){
+                TreeNode* root = q.front();
+                q.pop();
+                
+                row.push_back(root->val);
+                if(root->left) q.push(root->left);
+                if(root->right) q.push(root->right);
+            }
+            
+            if (level % 2 == 0) res.push_back(row);
+            else {
+                reverse(row.begin(), row.end());
+                res.push_back(row);
+            }
+            
+            
+            row.clear();
+            level += 1;
+        }
+        
+        
+        
+        return res;
+        
+    }
+};
+
