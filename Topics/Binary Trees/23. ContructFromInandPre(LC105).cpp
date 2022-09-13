3071777f08c4e10f22745c2814ee384b06f4e9c6
Shafi Khan <76864645+SaMs-Hub@users.noreply.github.com>
2022-09-14 00:55:10 +0530

    Update 23. ContructFromInandPre(LC105).cpp

--- Topics/Binary Trees/23. ContructFromInandPre(LC105).cpp
+++ Topics/Binary Trees/23. ContructFromInandPre(LC105).cpp
@@ -25,3 +25,27 @@ Node* constructTree(vector<int> &preOrder, vector<int> &inOrder){
 }
 
 
+// App 2 - Using maps
+unordered_map<int, int> inorderIndex;
+Node* constructTreeUtil(vector<int> &preorder, int &preorderIndex, vector<int> &inorder, int inorderStart, int inorderEnd) {
+	if (inorderStart > inorderEnd) {
+		return NULL;
+	}
+	Node* currNode = new Node(preorder[preorderIndex]);
+	preorderIndex++;
+	if (inorderStart == inorderEnd) {
+		return currNode;
+	}
+	int currInorderIndex = inorderIndex[currNode->data];
+	currNode->left = constructTreeUtil(preorder, preorderIndex, inorder, inorderStart, currInorderIndex - 1);
+	currNode->right = constructTreeUtil(preorder, preorderIndex, inorder, currInorderIndex + 1, inorderEnd);
+	return currNode;
+}
+Node* constructTree(vector<int> &preorder, vector<int> &inorder) {
+	inorderIndex.clear();
+    for (int i = 0; i < inorder.size(); i++){
+        inorderIndex[inorder[i]] = i;
+    }
+	int preorderIndex = 0;
+    return constructTreeUtil(preorder, preorderIndex, inorder, 0, inorder.size() - 1);
+}
