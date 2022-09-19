566d001e5e4ca9a75bee5f32e52cd3f3fb63ad2f
Shafi Khan <76864645+SaMs-Hub@users.noreply.github.com>
2022-09-20 00:33:37 +0530

    Update 33. RotateMatrix(LC48).cpp

--- Topics/Arrays/33. RotateMatrix(LC48).cpp
+++ Topics/Arrays/33. RotateMatrix(LC48).cpp
@@ -1,3 +1,4 @@
+// App 1- BF
 class Solution {
 public:
     void rotate(vector<vector<int>>& arr) {
@@ -24,3 +25,27 @@ public:
     }
     }
 };
+
+
+// App 2 - Same using Two pointers
+void rotate(vector<vector<int>> &arr)
+{
+	int n = arr.size();
+
+	for (int i = 0; i < n; i++)
+	{
+		for (int j = 0; j < i; j++)
+		{
+			swap(arr[i][j], arr[j][i]);
+		}
+	}
+
+	for (int i = 0; i < n; i++){
+		int left = 0, right = n - 1;
+		while (left < right){
+			swap(arr[i][left], arr[i][right]);
+			left += 1;
+			right -= 1;
+		}
+	}
+}
