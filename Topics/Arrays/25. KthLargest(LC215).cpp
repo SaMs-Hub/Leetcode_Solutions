4012b85a8ee7f4e9ddb2b04deb5aca3295d96db1
Shafi Khan <76864645+SaMs-Hub@users.noreply.github.com>
2022-09-21 00:37:16 +0530

    Update 25. KthLargest(LC215).cpp

--- Topics/Arrays/25. KthLargest(LC215).cpp
+++ Topics/Arrays/25. KthLargest(LC215).cpp
@@ -1,3 +1,5 @@
+// App 1 Using sort O(nlogn)
+
 class Solution {
 public:
     int findKthLargest(vector<int>& nums, int k) {
@@ -5,6 +7,17 @@ public:
         
         sort(nums.begin(), nums.end());
         return nums[k];
+        
+    }
+};
+
+// App 2 Maxheap priorityQue
+class Solution {
+public:
+    int findKthLargest(vector<int>& arr, int k) {
+       priority_queue<int> pr(arr.begin(), arr.end());
+	for (int i = 1;i < k; i++) pr.pop();
+	return pr.top();
         
     }
 };
