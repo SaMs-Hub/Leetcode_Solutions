adfebefd5fe4ea4e26dc67773743d33b00b743da
Shafi Khan <76864645+SaMs-Hub@users.noreply.github.com>
2022-09-21 14:35:14 +0530

    Update 23. MaxSubarray(LC53).cpp

--- Topics/Arrays/23. MaxSubarray(LC53).cpp
+++ Topics/Arrays/23. MaxSubarray(LC53).cpp
@@ -8,7 +8,7 @@ int maxSubArray(vector<int>& arr) {
 		return arr[0];
 	}
         int currSum = 0;
-        int maxSum = 0;
+        int maxSum = INT_MIN;
 
         for (int i =0; i < arr.size(); i++){
             currSum += arr[i];
