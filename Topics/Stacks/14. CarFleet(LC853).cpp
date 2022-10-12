9eebfea4610196d58a91cf6f73733b29e954a35d
Shafi Khan <76864645+SaMs-Hub@users.noreply.github.com>
2022-10-13 00:26:05 +0530

    Update 14. CarFleet(LC853).cpp

--- Topics/Stacks/14. CarFleet(LC853).cpp
+++ Topics/Stacks/14. CarFleet(LC853).cpp
@@ -27,3 +27,31 @@ int carFleet(int target, vector<int>& position, vector<int>& speed) {
 
 }
 };
+
+
+// App 2 - Using stack
+class Solution {
+public:
+int carFleet(int target, vector<int>& position, vector<int>& speed) {
+    stack<float> fleets;
+        vector<pair<int, float>> pos_time; 
+         
+        for(int i = 0; i < position.size(); i++)
+            pos_time.push_back({ position[i], float(target - position[i]) / speed[i] });
+        
+        sort(pos_time.begin(), pos_time.end());
+
+    
+        for(int i = 0; i < position.size(); i++){
+            float fleet_i = pos_time[i].second;
+         
+            while(fleets.size() && (fleet_i >= fleets.top()))
+                fleets.pop();
+            
+            fleets.push(fleet_i);
+        }
+        
+        return fleets.size();
+
+}
+};
