d263d495544ef1b06383c39ebf6d7d778e0d62d6
Shafi Khan <76864645+SaMs-Hub@users.noreply.github.com>
2022-10-21 23:26:04 +0530

    Create 5. LCA(LC235).cpp

---
+++ Topics/Binary Search Tree/5. LCA(LC235).cpp
@@ -0,0 +1,21 @@
+// App 1 Recursive O(n)
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+
+class Solution {
+public:
+     TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+    if (p->val < root->val && q->val < root->val) return lowestCommonAncestor(root->left, p, q);
+    else if(p->val > root->val && q->val > root->val) return lowestCommonAncestor(root->right, p, q);
+    return root;
+
+        
+ }
+};
