539d81025361297dec8f9d53a27b9807fb363be2
Shafi Khan <76864645+SaMs-Hub@users.noreply.github.com>
2022-10-26 00:07:42 +0530

    Create 5. ReverseInt(LC7).cpp

---
+++ Topics/BitManipulation/5. ReverseInt(LC7).cpp
@@ -0,0 +1,19 @@
+/// APp 1 Left Shing O(logx)
+class Solution {
+public:
+   
+ int reverse(int x) {
+    int rev = 0;
+    while (x != 0){
+        int remainder = x % 10;
+
+        if (rev > INT_MAX/10 or rev == INT_MAX/ 10 && remainder > 7) return 0;
+        if (rev < INT_MIN/10 or rev == INT_MIN/10 && remainder < -8) return 0;
+
+        x /= 10;
+        rev = rev * 10 + remainder;
+    }
+    return rev;
+        
+    }
+};
