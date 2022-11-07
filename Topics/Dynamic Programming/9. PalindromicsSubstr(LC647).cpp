74a2ae3cf49269cd59120d06a9fa92b5487c12c5
Shafi Khan <76864645+SaMs-Hub@users.noreply.github.com>
2022-11-07 22:21:09 +0530

    Create 9. PalindromicsSubstr(LC647).cpp

---
+++ Topics/Dynamic Programming/9. PalindromicsSubstr(LC647).cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+
+void middleOut(string s, int i, int j, int &result){
+    while (i >= 0 and j < s.size() and s[i] == s[j]){
+        result += 1;
+        i--;
+        j++;
+    }
+}
+
+ int countSubstrings(string s) {
+    int result = 0;
+
+    for (int i = 0; i < s.size(); i++){
+        middleOut(s, i, i, result);
+        middleOut(s, i, i + 1, result);
+    }
+
+    return result;
+    }
+};
