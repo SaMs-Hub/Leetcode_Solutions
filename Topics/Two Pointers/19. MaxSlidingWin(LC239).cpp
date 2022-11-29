32338816f24ff85dd7bc232b63ff97645295b404
Shafi Khan <76864645+SaMs-Hub@users.noreply.github.com>
2022-11-29 14:48:27 +0530

    Create 19. MaxSlidingWin(LC239).cpp

---
+++ Topics/Two Pointers/19. MaxSlidingWin(LC239).cpp
@@ -0,0 +1,27 @@
+// App 1 Deque O(n)
+
+ vector<int> maxSlidingWindow(vector<int>& nums, int k) {
+    int n = nums.size();
+    vector<int> result;
+    deque<int> deck;
+
+    int i = 0;
+    int j = 0;
+
+    while (j < n){
+        while (!deck.empty() && nums[deck.back()] < nums[j] ) deck.pop_back();
+        deck.push_back(j);
+
+        if (i > deck.front()) deck.pop_front();
+
+        if (j + 1 >= k){
+            result.push_back(nums[deck.front()]);
+            i++;
+        }
+
+        j++;
+    }
+
+    return result;
+        
+    }
