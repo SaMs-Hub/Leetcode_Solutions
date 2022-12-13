71e186ba37b99317be1d15fe2d4f773171e395de
Shafi Khan <76864645+SaMs-Hub@users.noreply.github.com>
2022-12-13 14:48:48 +0530

    Create 1. ImplementTrie(LC208).cpp

---
+++ Topics/Trie/1. ImplementTrie(LC208).cpp
@@ -0,0 +1,68 @@
+// App 1 O(n) for all operations
+
+class TrieNode {
+public:
+    TrieNode* children[26];
+    bool isWord;
+    
+    TrieNode() {
+        for (int i = 0; i < 26; i++) {
+            children[i] = NULL;
+        }
+        isWord = false;
+    }
+};
+
+class Trie {
+public:
+    Trie() {
+        root = new TrieNode();
+    }
+    
+    void insert(string word) {
+        TrieNode* node = root;
+        int curr = 0;
+        
+        for (int i = 0; i < word.size(); i++) {
+            curr = word[i] - 'a';
+            if (node->children[curr] == NULL) {
+                node->children[curr] = new TrieNode();
+            }
+            node = node->children[curr];
+        }
+        
+        node->isWord = true;
+    }
+    
+    bool search(string word) {
+        TrieNode* node = root;
+        int curr = 0;
+        
+        for (int i = 0; i < word.size(); i++) {
+            curr = word[i] - 'a';
+            if (node->children[curr] == NULL) {
+                return false;
+            }
+            node = node->children[curr];
+        }
+        
+        return node->isWord;
+    }
+    
+    bool startsWith(string prefix) {
+        TrieNode* node = root;
+        int curr = 0;
+        
+        for (int i = 0; i < prefix.size(); i++) {
+            curr = prefix[i] - 'a';
+            if (node->children[curr] == NULL) {
+                return false;
+            }
+            node = node->children[curr];
+        }
+        
+        return true;
+    }
+private:
+    TrieNode* root;
+};
