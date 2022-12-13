ad37eee88fda91e9e093be606cf1c74c2b010a9e
Shafi Khan <76864645+SaMs-Hub@users.noreply.github.com>
2022-12-13 22:58:55 +0530

    Create 2. DesignAddSearch(LC211).cpp

---
+++ Topics/Trie/2. DesignAddSearch(LC211).cpp
@@ -0,0 +1,60 @@
+// O(m *26)
+class TrieNode {
+public:
+    TrieNode* children[26];
+    bool isWord;
+    
+    TrieNode() {
+        for (int i = 0; i < 26; i++) {
+            children[i] = NULL;
+        }
+        isWord = false;
+    }
+};
+
+class WordDictionary {
+public:
+    WordDictionary() {
+        root = new TrieNode();
+    }
+    
+    void addWord(string word) {
+        TrieNode* node = root;
+        int curr = 0;
+        
+        for (int i = 0; i < word.size(); i++) {
+            curr = word[i] - 'a';
+            if (node->children[curr] == NULL) {
+                node->children[curr] = new TrieNode();
+            }
+            node = node->children[curr];
+        }
+        
+        node->isWord = true;
+    }
+    
+    bool search(string word) {
+        TrieNode* node = root;
+        return searchInNode(word, 0, node);
+    }
+private:
+    TrieNode* root;
+    
+    bool searchInNode(string& word, int i, TrieNode* node) {
+        if (node == NULL) {
+            return false;
+        }
+        if (i == word.size()) {
+            return node->isWord;
+        }
+        if (word[i] != '.') {
+            return searchInNode(word, i + 1, node->children[word[i] - 'a']);
+        }
+        for (int j = 0; j < 26; j++) {
+            if (searchInNode(word, i + 1, node->children[j])) {
+                return true;
+            }
+        }
+        return false;
+    }
+};
