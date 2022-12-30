dd74271a5ee087810a3d923681979379cb63d29e
Shafi Khan <76864645+SaMs-Hub@users.noreply.github.com>
2022-12-30 23:59:44 +0530

    Create 11. DesignTwitter(LC355).cpp

---
+++ Topics/Queues/11. DesignTwitter(LC355).cpp
@@ -0,0 +1,47 @@
+// App 1 O(n)
+
+    Twitter() {
+        
+    }
+    
+    void postTweet(int userId, int tweetId) {
+        posts.push_back({userId, tweetId});
+    }
+    
+    vector<int> getNewsFeed(int userId) {
+        // 10 tweets
+        int count = 10;
+        vector<int> result;
+        
+        // since postTweet pushes to the back, looping from back gets most recent
+        for (int i = posts.size() - 1; i >= 0; i--) {
+            if (count == 0) {
+                break;
+            }
+            
+            int followingId = posts[i].first;
+            int tweetId = posts[i].second;
+            unordered_set<int> following = followMap[userId];
+            // add to result if they're following them or it's a tweet from themself
+            if (following.find(followingId) != following.end() || followingId == userId) {
+                result.push_back(tweetId);
+                count--;
+            }
+        }
+        
+        return result;
+    }
+    
+    void follow(int followerId, int followeeId) {
+        followMap[followerId].insert(followeeId);
+    }
+    
+    void unfollow(int followerId, int followeeId) {
+        followMap[followerId].erase(followeeId);
+    }
+private:
+    // pairs: [user, tweet]
+    vector<pair<int, int>> posts;
+    // hash map: {user -> people they follow}
+    unordered_map<int, unordered_set<int>> followMap;
+};
