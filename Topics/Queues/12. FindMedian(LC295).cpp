d17ddc93dee55533f379eb5ebe3486ed018590b7
Shafi Khan <76864645+SaMs-Hub@users.noreply.github.com>
2023-01-08 22:02:56 +0530

    Create 12. FindMedian(LC295).cpp

---
+++ Topics/Queues/12. FindMedian(LC295).cpp
@@ -0,0 +1,54 @@
+// APp 1 Max + min heap O(log n) + O(1)
+class MedianFinder {
+public:
+   	priority_queue<int> lower;
+	priority_queue<int, vector<int>, greater<int>> higher;
+	MedianFinder()
+	{
+	}
+
+	void addNum(int num)
+	{
+		if (lower.empty()){
+			lower.push(num);
+			return;
+		}
+
+		if (lower.size() > higher.size()){
+			if (lower.top() > num){
+				higher.push(lower.top());
+				lower.pop();
+				lower.push(num);
+
+			}else higher.push(num);
+
+		}else{
+			if (num > higher.top()){
+				lower.push(higher.top());
+				higher.pop();
+				higher.push(num);
+			}else lower.push(num);
+
+		}
+	}
+
+	double findMedian()
+	{
+		double result = 0.0;
+
+		if (lower.size() == higher.size()) result = lower.top() + (higher.top() - lower.top())/2.0;
+		else{
+			if (lower.size() > higher.size()) result = lower.top();
+			else result = higher.top();
+		}
+
+		return result;
+	}
+};
+
+/**
+ * Your MedianFinder object will be instantiated and called as such:
+ * MedianFinder* obj = new MedianFinder();
+ * obj->addNum(num);
+ * double param_2 = obj->findMedian();
+ */
