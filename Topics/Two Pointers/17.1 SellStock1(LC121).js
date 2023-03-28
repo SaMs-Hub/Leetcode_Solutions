3b0a900a0afeca8c5cda8744a2a4c3528a9aa315
Shafi Khan <76864645+SaMs-Hub@users.noreply.github.com>
2023-03-28 11:49:13 +0530

    Update 17.1 SellStock1(LC121).js

--- Topics/Two Pointers/17.1 SellStock1(LC121).js
+++ Topics/Two Pointers/17.1 SellStock1(LC121).js
@@ -12,3 +12,16 @@ var maxProfit = function(arr) {
 
 
 // Two pointers
+var maxProfit = function(arr) {
+   let [i, j, maxProfit] = [0, 1, 0];
+
+   while (j <= arr.length){
+       if (arr[i] < arr[j]){
+           maxProfit = Math.max(maxProfit, arr[j] - arr[i]);
+       }else i = j;
+
+       j++;
+   }
+
+   return maxProfit;
+};
