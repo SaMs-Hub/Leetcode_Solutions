720f3f8961dfc5475b942fb6c333bf31e569dd5b
Shafi Khan <76864645+SaMs-Hub@users.noreply.github.com>
2023-04-02 15:22:22 +0530

    Update 16. Search(LC704).cpp

--- Topics/Searching/16. Search(LC704).cpp
+++ Topics/Searching/16. Search(LC704).cpp
@@ -1,3 +1,5 @@
+
+// App1 Recursion
 int bSearch(vector<int>& nums, int target, int start, int end){
         if(end < start) return -1;
         int mid = (start+end)/2;
@@ -13,4 +15,21 @@ int bSearch(vector<int>& nums, int target, int start, int end){
     
     int search(vector<int>& nums, int target) {
         return bSearch(nums, target, 0, nums.size()-1);
+    }
+
+
+// Two pointers
+int search(vector<int>& arr, int target) {
+        int i = 0;
+        int j = arr.size() - 1;
+
+        while (i <= j){
+            int mid = (i + j)/2;
+
+            if (arr[mid] == target) return mid;
+            else if (arr[mid] < target) i = mid +1;
+            else j = mid - 1;
+        }
+
+        return -1;
     }
