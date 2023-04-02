b45ab133b8921105dcf9a28b3aa95613580a7ec5
Shafi Khan <76864645+SaMs-Hub@users.noreply.github.com>
2023-04-02 14:58:42 +0530

    Create 3.1 ValidPar(LC20).js

---
+++ Topics/Stacks/3.1 ValidPar(LC20).js
@@ -0,0 +1,30 @@
+
+var isValid = function(str) {
+      let stack = [];
+    let map = {
+        '}': '{',
+        ']': '[',
+        ')': '(',
+    }
+
+    for (let x of str) {
+
+        let isPresent = x in map;
+        if (!isPresent) {
+            stack.push(x);
+            continue;
+        }
+
+        console.log(stack, x, map[x])
+
+        let isEqual = stack[stack.length - 1] === map[x];
+        if (isEqual) {
+            stack.pop();
+            continue;
+        }
+
+        return false;
+    }
+
+    return stack.length === 0;
+};
