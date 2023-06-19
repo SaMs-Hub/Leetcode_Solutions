828b7a336ae7187ca7f8d2834b93e2121cec4ba5
Shafi Khan <76864645+SaMs-Hub@users.noreply.github.com>
2023-06-19 14:43:30 +0530

    Update 17.1 RunningSum(LC1480).js

--- Topics/Arrays/17.1 RunningSum(LC1480).js
+++ Topics/Arrays/17.1 RunningSum(LC1480).js
@@ -1,4 +1,4 @@
-// O(n)
+// App 1  O(n)
 var runningSum = function(nums) {
     let result = [];
     result.push(nums[0]); 
@@ -8,4 +8,13 @@ var runningSum = function(nums) {
 
     return result;
     
+};
+
+// App 2 Using same array
+var runningSum = function(arr) {
+    for (let i = 1; i < arr.length; ++i) {
+    arr[i] += arr[i - 1];
+  }
+
+  return arr;
 };
