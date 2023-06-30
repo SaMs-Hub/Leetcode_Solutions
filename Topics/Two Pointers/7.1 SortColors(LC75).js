48f4bbb19ff5c530b79c9009157e3b2a7b832014
Shafi Khan <76864645+SaMs-Hub@users.noreply.github.com>
2023-07-01 02:18:24 +0530

    Update 7.1 SortColors(LC75).js

--- Topics/Two Pointers/7.1 SortColors(LC75).js
+++ Topics/Two Pointers/7.1 SortColors(LC75).js
@@ -1,21 +1,19 @@
-var sortColors = function(arr) {
-    let low = 0;
-    let mid = 0;
-    let high = arr.length - 1;
+// App1 - O(n)
+// logic used: try put elements in [low, medium, high] format
+const sortColors = (arr) => {
+    let [low, medium, high] = [0, 0, arr.length - 1];
 
-    while (mid <= high){
-        if (arr[mid] == 0){
-            [arr[mid], arr[low]] = [arr[low], arr[mid]];
+    while (medium <= high) {
+        if (arr[medium] === 0) {
+            [arr[medium], arr[low]] = [arr[low], arr[medium]];
             low += 1;
-            mid += 1;
-        }else if(arr[mid] == 1){
-            mid += 1;
-        }else{
-            [arr[mid], arr[high]] = [arr[high], arr[mid]]
+            medium += 1;
+        } else if (arr[medium] === 1) medium += 1;
+        else {
+            [arr[medium], arr[high]] = [arr[high], arr[medium]];
             high -= 1;
         }
     }
-
     return arr;
-    
-};
+
+}
