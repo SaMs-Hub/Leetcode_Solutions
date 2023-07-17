973dfd855dc2c3dc41f11f43742638e8b70bdb85
Shafi Khan <76864645+SaMs-Hub@users.noreply.github.com>
2023-07-17 14:41:48 +0530

    Update 8.1 MergeTwoLists(LC21).js

--- Topics/Linked Lists/8.1 MergeTwoLists(LC21).js
+++ Topics/Linked Lists/8.1 MergeTwoLists(LC21).js
@@ -1,15 +1,13 @@
-// Using recursion
+// Using recursion - O(n)
+const mergeTwoLists = (l1, l2) => {
+    if (!l1) return l2;
+    if (!l2) return l1;
 
-var mergeTwoLists = function(list1, list2) {
-    if (!list1) return list2;
-    if (!list2) return list1;
-
-    if (list1.val <= list2.val){
-        list1.next = mergeTwoLists(list1.next, list2);
-        return list1;
+    if (l1.val <= l2.val){
+        l1.next = mergeTwoLists(l1.next, l2);
+        return l1;
     }else{
-        list2.next = mergeTwoLists(list1, list2.next);
-        return list2;
+        l2.next = mergeTwoLists(l1, l2.next);
+        return l2;
     }
-    
-};
+}
