0ef813edc46ebdd305955f3eca2f071e9a16a5a8
Shafi Khan <76864645+SaMs-Hub@users.noreply.github.com>
2023-07-17 14:52:55 +0530

    Create 32.1 HasCycle(LC141).js

---
+++ Topics/Linked Lists/32.1 HasCycle(LC141).js
@@ -0,0 +1,16 @@
+// Two pointers - O(n)
+const hasCycle = (head) => {
+    if (!head || !head.next) return false;
+    else{
+        let [slow, fast] = [head, head.next];
+
+        while (slow != fast){
+            if (!fast || !fast.next) return false;
+            else{
+                slow = slow.next;
+                fast = fast.next.next; 
+            }
+        }
+        return true;
+    }
+}
