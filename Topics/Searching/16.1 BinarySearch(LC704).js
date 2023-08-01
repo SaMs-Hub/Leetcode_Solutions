9eef4ca5576c2c4f60ad8acdf565c5ad8387c58c
Shafi Khan <76864645+SaMs-Hub@users.noreply.github.com>
2023-08-01 14:26:18 +0530

    Update 16.1 BinarySearch(LC704).js

--- Topics/Searching/16.1 BinarySearch(LC704).js
+++ Topics/Searching/16.1 BinarySearch(LC704).js
@@ -1,14 +1,13 @@
 // Two pointers
 const search = (arr, target) => {
-    let [i, j] = [0, arr.length - 1];
-
-    while (i <= j) {
-        let mid = (i + j) >> 1;
-        if (arr[mid] === target) return mid;
-        else if (arr[mid] < target) i = mid + 1;
-        else j = mid - 1;
+    let [start, end] = [0, arr.length - 1];
+    
+    while (start <= end){
+        let mid = (start + end) >> 1;
+        if (arr[mid] == target) return mid;
+        else if(arr[mid] < target) start = mid + 1;
+        else end = mid - 1;
     }
-
+    
     return -1;
-
 }
