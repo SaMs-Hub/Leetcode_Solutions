0867f63c09b0e9fe6a8f500617ec264ea3c342c5
Shafi Khan <76864645+SaMs-Hub@users.noreply.github.com>
2023-10-06 08:35:30 +0530

    Update 3.1 TwoSum(LC1).js

--- Topics/Two Pointers/3.1 TwoSum(LC1).js
+++ Topics/Two Pointers/3.1 TwoSum(LC1).js
@@ -1,3 +1,18 @@
+// App 1 Brute force O(n**2)
+const twoSum = (arr, target) => {
+   
+    const n = arr.length;
+    for (let i = 0; i < n; i++){
+        for (let j = i + 1; j < n; j++){
+            if (arr[i] + arr[j] == target) return [i, j];
+        }
+    }
+    return -1;
+}
+
+
+
+
 // Two pointers O(n)
 // using hashmap - works with unsorted as well
 var twoSum = function(arr, target) {
