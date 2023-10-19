6cd10dfbcd450c607deb8938801030c833a0b5c8
Shafi Khan <76864645+SaMs-Hub@users.noreply.github.com>
2023-10-19 08:29:04 +0530

    Update 7.1 SortColors(LC75).js

--- Topics/Two Pointers/7.1 SortColors(LC75).js
+++ Topics/Two Pointers/7.1 SortColors(LC75).js
@@ -2,18 +2,19 @@
 // logic used: try put elements in [low, medium, high] format
 const sortColors = (arr) => {
     let [low, medium, high] = [0, 0, arr.length - 1];
-
-    while (medium <= high) {
-        if (arr[medium] === 0) {
+    
+    while (medium <= high){
+        if (arr[medium] === 0){
             [arr[medium], arr[low]] = [arr[low], arr[medium]];
-            low += 1;
             medium += 1;
-        } else if (arr[medium] === 1) medium += 1;
-        else {
+            low += 1;
+        }else if(arr[medium] === 1) medium += 1;
+        else{
             [arr[medium], arr[high]] = [arr[high], arr[medium]];
-            high -= 1;
+            high -=1 ;
         }
     }
+    
+    
     return arr;
-
 }
