90a17403f00af249bdadc28bbe65f99b6cb4e90e
Shafi Khan <76864645+SaMs-Hub@users.noreply.github.com>
2023-10-23 00:41:38 +0530

    Update 23.1 MaxSubArray(LC53).js

--- Topics/Arrays/23.1 MaxSubArray(LC53).js
+++ Topics/Arrays/23.1 MaxSubArray(LC53).js
@@ -1,20 +1,21 @@
 // Greedy - O(n)
-const maxSubArray = (arr) => {
-    let n = arr.length;
-    
+var maxSubArray = function(arr) {
+
+   let n = arr.length;
+
     if (n == 1) return arr[0];
-    
+
     let [currentSum, maxSum] = [arr[0], arr[0]];
-    
-    for (let i = 1; i < n; i++){
-        currentSum = Math.max(currentSum, arr[i] + currentSum);
+
+    for (let i = 1; i < n; i++) {
+        currentSum = Math.max(arr[i], arr[i] + currentSum);
         maxSum = Math.max(maxSum, currentSum);
     }
-    
-    return maxSum;
-    
-}
 
+
+
+    return maxSum;
+};
 // BF 
 
 var maxSubArray = function(arr) {
