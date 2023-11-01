855b7d50ece65905edc5e69fc5eb4574f0cc0a1c
Shafi Khan <76864645+SaMs-Hub@users.noreply.github.com>
2023-11-01 23:37:28 +0530

    Update 32.1 HasCycle(LC141).js

--- Topics/Linked Lists/32.1 HasCycle(LC141).js
+++ Topics/Linked Lists/32.1 HasCycle(LC141).js
@@ -1,16 +1,19 @@
 // Two pointers - O(n)
 const hasCycle = (head) => {
-    if (!head || !head.next) return false;
-    else{
-        let [slow, fast] = [head, head.next];
-
-        while (slow != fast){
-            if (!fast || !fast.next) return false;
-            else{
-                slow = slow.next;
-                fast = fast.next.next; 
-            }
-        }
-        return true;
+  if (!head || !head.next) return false;
+  else {
+    let [slow, fast] = [head, head.next];
+    while (slow != fast) {
+      if (!fast || !fast.next) return false;
+      else {
+        slow = slow.next;
+        fast = fast.next.next;
+      }
     }
-}
+
+    return true;
+  }
+};
+
+
+url: https://leetcode.com/problems/linked-list-cycle/description/
