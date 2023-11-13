481e15ec8858f2ebfa4642cfce86b8d684666c11
Shafi Khan <76864645+SaMs-Hub@users.noreply.github.com>
2023-11-13 23:10:03 +0530

    Update 21.2 FindInter(LC160).js

--- Topics/Linked Lists/21.2 FindInter(LC160).js
+++ Topics/Linked Lists/21.2 FindInter(LC160).js
@@ -1,14 +1,12 @@
+// App 1 - While Loop
 var getIntersectionNode = function(headA, headB) {
-   
-    let f2;
-
-    while (headA != null){
-        f2 = headB
-        while (f2 != null){
-            if (headA == f2) return headA;
-            f2 = f2.next;
+    let tempHead;
+    while (headA){
+        tempHead = headB
+        while (tempHead){
+            if (headA == tempHead) return headA;
+            tempHead = tempHead.next;
         }
-
         headA = headA.next;
     }
 
