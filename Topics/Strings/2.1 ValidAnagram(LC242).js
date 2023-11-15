9924298a7975d0bab429354ec35558cf59cba9a2
Shafi Khan <76864645+SaMs-Hub@users.noreply.github.com>
2023-11-15 09:09:48 +0530

    Update 2.1 ValidAnagram(LC242).js

--- Topics/Strings/2.1 ValidAnagram(LC242).js
+++ Topics/Strings/2.1 ValidAnagram(LC242).js
@@ -5,21 +5,28 @@ var isAnagram = function(s, t) {
 
 
 // App 2 Using maps O(n)
-var isAnagram = function(s, t) {
-    if (s.length !== t.length) return false;
-    const map = new Map();
+const isAnagram = (s, t) => {
+  // base case
+  if (s.length !== t.length) return false;
 
-    for (let x of s){
-        if (map.has(x)) map.set(x, map.get(x) + 1);
-        else map.set(x, 1)
-    }
+  // creating a map to store values( can also use object);
+  const myMap = new Map();
 
-    for (let x of t){
-        if (!map.has(x)) return false;
-        map.set(x, map.get(x) - 1);
-        if (map.get(x) === 0) map.delete(x);
-    }
+  // inserting everything of s into map;
+  for (let x of s) {
+    if (myMap.has(x)) myMap.set(x, myMap.get(x) + 1);
+    else myMap.set(x, 1);
+  }
 
-    if (map.size > 0) return false;
-    return true;
+  // using other word to deal with existing words
+  for (let x of t) {
+    if (!myMap.has(x)) return false;
+    else myMap.set(x, myMap.get(x) - 1);
+    if (myMap.get(x) === 0) myMap.delete(x);
+  }
+
+  return myMap.size === 0;
 };
+
+
+https://leetcode.com/problems/valid-anagram/description/
