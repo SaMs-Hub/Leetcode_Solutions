b254844609222844b50da230089d0484239837db
Shafi Khan <76864645+SaMs-Hub@users.noreply.github.com>
2023-11-21 23:47:46 +0530

    Create 2.1 MergeIntervals(LC56).js

---
+++ Topics/Intervals/2.1 MergeIntervals(LC56).js
@@ -0,0 +1,23 @@
+// App 1
+const merge = (intervals) => {
+  const result = [];
+  let currentCount = 0;
+  let totalIntervals = intervals.length;
+  // sorting based on 1st value
+  intervals.sort((a, b) => a[0] - b[0]);
+
+  while (currentCount < totalIntervals - 1) {
+    if (intervals[currentCount][1] >= intervals[currentCount + 1][0]) {
+      intervals[currentCount + 1][0] = intervals[currentCount][0];
+      intervals[currentCount + 1][1] = Math.max(
+        intervals[currentCount + 1][1],
+        intervals[currentCount][1]
+      );
+    } else result.push(intervals[currentCount]);
+
+    currentCount += 1;
+  }
+  result.push(intervals[currentCount]);
+
+  return result;
+};
