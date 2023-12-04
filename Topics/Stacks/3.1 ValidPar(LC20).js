f99e90471cd2505c6b1a480a6dcbd80413f8c62d
Shafi Khan <76864645+SaMs-Hub@users.noreply.github.com>
2023-12-04 18:20:48 +0530

    Update 3.1 ValidPar(LC20).js

--- Topics/Stacks/3.1 ValidPar(LC20).js
+++ Topics/Stacks/3.1 ValidPar(LC20).js
@@ -1,28 +1,32 @@
-
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
 const isValid = (str) => {
-    const stack = [];
-    const myMap = {
-        "}": "{",
-        "]": "[",
-        ")": "("
+     const stack = [];
+  const myMap = {
+    ")": "(",
+    "}": "{",
+    "]": "[",
+  };
+
+  for (let x of str) {
+    const isPresent = x in myMap;
+
+    if (!isPresent) {
+      stack.push(x);
+      continue;
     }
-    
-    for (let x of str){
-        const isPresent = x in myMap;
-        if (!isPresent){
-            stack.push(x);
-            continue;
-        }
-        
-        const isEqual = stack[stack.length - 1] === myMap[x];
-        
-        if (isEqual){
-            stack.pop();
-            continue;
-        }
-        
-        return false;
+
+    const isEqual = stack[stack.length - 1] === myMap[x];
+
+    if (isEqual) {
+      stack.pop();
+      continue;
     }
-    
-    return stack.length === 0;
+
+    return false;
+  }
+
+  return stack.length === 0;
 }
