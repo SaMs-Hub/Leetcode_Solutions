cddd5b3ed304e8c0befdad872f4a2df7403bbbbb
Shafi Khan <76864645+SaMs-Hub@users.noreply.github.com>
2023-12-08 00:31:34 +0530

    Update 45. SortArray(LC912).js

--- Topics/Arrays/45. SortArray(LC912).js
+++ Topics/Arrays/45. SortArray(LC912).js
@@ -18,4 +18,41 @@ var sortArray = function(arr) {
     
 };
 
-//
+// Bubble Sort - O(n2)
+var sortArray = function(arr) {
+   
+   let n = arr.length;
+
+  for (let i = 1; i <= n; i++) {
+    for (let j = 0; j <= n - i - 1; j++) {
+      if (arr[j] > arr[j + 1]) {
+        [arr[j], arr[j + 1]] = [arr[j + 1], arr[j]];
+      }
+    }
+  }
+  return arr;
+
+    
+};
+
+// Insertion sort
+var sortArray = function(arr) {
+  let n = arr.length;
+
+  for (let i = 1; i < n; i++) {
+    let currentElement = arr[i];
+    let j = i - 1;
+
+    while (j >= 0 && arr[j] > currentElement) {
+      arr[j + 1] = arr[j];
+      j -= 1;
+    }
+    arr[j + 1] = currentElement;
+  }
+
+  return arr;
+};
+
+
+// 
+
