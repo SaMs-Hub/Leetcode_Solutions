4741e014820f984f4f3bccfdfef3f194cbc9507d
Shafi Khan <76864645+SaMs-Hub@users.noreply.github.com>
2023-12-12 02:07:18 +0530

    Update 22.1 MaxOnes(LC485).js

--- Topics/Arrays/22.1 MaxOnes(LC485).js
+++ Topics/Arrays/22.1 MaxOnes(LC485).js
@@ -5,11 +5,13 @@ var findMaxConsecutiveOnes = function(arr) {
     for (let i = 0; i < arr.length; i++){
         if (arr[i] === 1) currentCount += 1;
         else {
-            maxCount = Math.max(currentCount, maxCount);
             currentCount = 0;
         }
+           maxCount = Math.max(currentCount, maxCount);
     }
     
-     maxCount = Math.max(currentCount, maxCount);
+    
      return maxCount;
 };
+
+https://leetcode.com/problems/max-consecutive-ones/
