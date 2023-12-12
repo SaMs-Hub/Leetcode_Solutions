17e0936fe73431626f622317340d5d24399b1c6a
Shafi Khan <76864645+SaMs-Hub@users.noreply.github.com>
2023-12-12 11:33:29 +0530

    Update 7.1 SortColors(LC75).js

--- Topics/Two Pointers/7.1 SortColors(LC75).js
+++ Topics/Two Pointers/7.1 SortColors(LC75).js
@@ -1,20 +1,29 @@
 // App1 - O(n)
 // logic used: try put elements in [low, medium, high] format
+
+const swapChar = (arr, a, b) => {
+  [arr[a], arr[b]] = [arr[b], arr[a]];
+};
+
 const sortColors = (arr) => {
-    let [low, medium, high] = [0, 0, arr.length - 1];
-    
-    while (medium <= high){
-        if (arr[medium] === 0){
-            [arr[medium], arr[low]] = [arr[low], arr[medium]];
-            medium += 1;
-            low += 1;
-        }else if(arr[medium] === 1) medium += 1;
-        else{
-            [arr[medium], arr[high]] = [arr[high], arr[medium]];
-            high -=1 ;
-        }
+  let n = arr.length;
+
+  let [low, medium, high] = [0, 0, n - 1];
+
+  while (medium <= high) {
+    if (arr[medium] === 0) {
+      swapChar(arr, medium, low);
+      low += 1;
+      medium += 1;
+    } else if (arr[medium] === 1) {
+      medium += 1;
+    } else {
+      swapChar(arr, medium, high);
+      high -= 1;
     }
-    
-    
-    return arr;
-}
+  }
+
+  return arr;
+};
+
+// https://leetcode.com/problems/sort-colors/
