23dd5f1cda358cecc4367d624cb53c912b4ea552
Shafi Khan <76864645+SaMs-Hub@users.noreply.github.com>
2024-01-19 00:12:35 +0530

    Update 21. LongSubStr(LC3).js

--- Topics/Two Pointers/21. LongSubStr(LC3).js
+++ Topics/Two Pointers/21. LongSubStr(LC3).js
@@ -1,22 +1,29 @@
 // O(n)
 
-const lengthOfLongestSubstring = (str) => {
-  let n = str.length;
 
-  const set = new Set();
-  let [left, right, max] = [0, 0, 0];
+var lengthOfLongestSubstring = function(str) {
+     const length = str.length;
+        const set = new Set();
 
-  while (right < n) {
-    while (set.has(str[right])) {
-      set.delete(str[left]);
-      left += 1;
-    }
-    set.add(str[right]);
-    max = Math.max(max, set.size);
-    right += 1;
-  }
+        let left = 0;
+        let right = 0;
+        let max = 0;
 
-  return max;
+        while (right < length){
+            // delete all instances if presetn
+            while (set.has(str[right])){
+                set.delete(str[left]);
+                left += 1;
+            }
+
+            // Adding char to set & check max size
+            set.add(str[right]);
+            max = Math.max(max, set.size);
+            right += 1;
+
+        }
+
+        return max;
+    
 };
-
 // https://leetcode.com/problems/longest-substring-without-repeating-characters/
