6b20c5852d9a36ec224c0a8666028794174517a9
Shafi Khan <76864645+SaMs-Hub@users.noreply.github.com>
2024-01-22 23:47:41 +0530

    Update 8.1 LongestPalSubStr(LC5).js

--- Topics/Strings/8.1 LongestPalSubStr(LC5).js
+++ Topics/Strings/8.1 LongestPalSubStr(LC5).js
@@ -1,27 +1,36 @@
-var longestPalindrome = function(str) {
-     let [start, maxLength] = [0, 0];
-    if (str.length < 2) return str;
-    
-    let low, high;
-    
-    for (let i = 0; i < str.length; i += 0.5){
-        low = Math.floor(i);
-        high = Math.ceil(i);
-        
-        while (low >= 0 && high < str.length && str[low] === str[high]){
 
-            low -= 1;
-            high += 1;
-        }
-        
-        let length = high - low - 1;
-       
-        if (length > maxLength){
-            maxLength = length;
-            start = low + 1;
-        }
+const longestPalindrome = (str) => {
+  let start = 0;
+  let maxLength = 0;
 
-        
-    }
-            return str.substring(start, start + maxLength);
-};
+  let n = str.length;
+
+  if (n < 2){
+      return str;
+  }
+
+  let low;
+  let high;
+
+  for (let i = 0; i < n; i += 0.5){
+      low = Math.floor(i);
+      high = Math.ceil(i);
+
+      while (low >= 0 && high < n && str[low] === str[high]){
+          low -= 1;
+          high += 1;
+      }
+
+      let length = high - low - 1;
+
+      if (length > maxLength){
+          maxLength = length;
+          start = low + 1;
+      }
+  }
+
+  return str.substring(start, start + maxLength);
+
+}
+
+// https://leetcode.com/problems/longest-palindromic-substring/
