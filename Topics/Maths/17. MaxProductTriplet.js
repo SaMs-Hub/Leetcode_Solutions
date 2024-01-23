e3e1abff0df3efec03cafc1bf709e318972403d5
Shafi Khan <76864645+SaMs-Hub@users.noreply.github.com>
2024-01-24 00:49:34 +0530

    Create 17. MaxProductTriplet.js

---
+++ Topics/Maths/17. MaxProductTriplet.js
@@ -0,0 +1,24 @@
+// BF O(n3)
+const maxProduct = (arr) => {
+  let maxValue = Number.MIN_VALUE;
+  let n = arr.length;
+  for (let i = 0; i < n; i++) {
+    for (let j = i + 1; j < n; j++) {
+      for (let k = j + 1; k < n; k++) {
+        const currentProduct = arr[i] * arr[j] * arr[k];
+        if (currentProduct > maxValue) {
+          maxValue = Math.max(maxValue, currentProduct);
+        }
+      }
+    }
+  }
+
+  return maxValue;
+};
+
+const arr = [8, 2, 8, 1, -4];
+console.log(maxProduct(arr));
+
+
+
+
