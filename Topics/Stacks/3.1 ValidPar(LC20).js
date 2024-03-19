2b4e9ca60dc612bbdd92153bb0d971d4f2d9464c
Shafi Khan <76864645+SaMs-Hub@users.noreply.github.com>
2024-03-19 23:49:45 +0530

    Update 3.1 ValidPar(LC20).js

--- Topics/Stacks/3.1 ValidPar(LC20).js
+++ Topics/Stacks/3.1 ValidPar(LC20).js
@@ -31,4 +31,37 @@ const isValid = (str) => {
   return stack.length === 0;
 }
 
+
+// BF
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+var isValid = function(str) {
+ const n = str.length;
+  const myStack = [];
+
+  const obj = {
+    "}": "{",
+    "]": "[",
+    ")": "(",
+  };
+
+  for (let i = 0; i < n; i += 1) {
+    if (str[i] in obj) {
+      console.log(obj[str[i]]);
+      if (myStack[myStack.length - 1] === obj[str[i]]) {
+        myStack.pop();
+      } else {
+        return false;
+      }
+    } else {
+      myStack.push(str[i]);
+    }
+  }
+
+  return myStack.length === 0;
+    
+};
+
 https://leetcode.com/problems/valid-parentheses/description/
