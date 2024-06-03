c0353cfab86d7e206ee2035b28c0a7524a712aa4
Shafi Khan <76864645+SaMs-Hub@users.noreply.github.com>
2024-06-03 21:27:31 +0530

    Update 38.1 SwapNodes(LC24).js

--- Topics/Linked Lists/38.1 SwapNodes(LC24).js
+++ Topics/Linked Lists/38.1 SwapNodes(LC24).js
@@ -1,9 +1,10 @@
 // Recursive soln
 const swapPairs = (head) => {
-  if (head === null || head.next === null) return head;
+  if (head === null || head.next === null) {
+    return head;
+  }
 
   let currentNode = head.next;
-
   head.next = swapPairs(head.next.next);
 
   currentNode.next = head;
