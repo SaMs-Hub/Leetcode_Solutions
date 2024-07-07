f278c973ff22c897bed3c0b44e8ff8142ebd1add
Shafi Khan <76864645+SaMs-Hub@users.noreply.github.com>
2024-07-08 02:03:26 +0530

    Update 47. SecondMax.js

--- Topics/Arrays/47. SecondMax.js
+++ Topics/Arrays/47. SecondMax.js
@@ -19,3 +19,26 @@ const secondMax = (arr) => {
 
 console.log(secondMax(arr));
 
+
+// O(n)
+
+const secondMax = (arr) => {
+  let [first, second] = Array(2).fill(-Infinity);
+
+  const filteredArray = [...new Set(arr)];
+
+  for (let currentNum of filteredArray) {
+    if (currentNum > first) {
+      [first, second] = [currentNum, first];
+    } else if (currentNum > second) {
+      second = currentNum;
+    }
+  }
+
+  if (second === -Infinity) {
+    return first;
+  } else {
+    return second;
+  }
+};
+
