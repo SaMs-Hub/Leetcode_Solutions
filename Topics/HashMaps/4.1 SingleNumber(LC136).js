b01045e0c545ca6231aa05ad4b76195edcc283a1
Shafi Khan <76864645+SaMs-Hub@users.noreply.github.com>
2024-07-09 00:01:59 +0530

    Create 4.1 SingleNumber(LC136).js

---
+++ Topics/HashMaps/4.1 SingleNumber(LC136).js
@@ -0,0 +1,22 @@
+// O(n)
+const findUnique = (arr) => {
+  const occurences = {};
+
+  for (let currentNum of arr) {
+    if (occurences[currentNum]) {
+      occurences[currentNum] = occurences[currentNum] + 1;
+    } else {
+      occurences[currentNum] = 1;
+    }
+  }
+
+  let result;
+  for (let key in occurences) {
+    if (occurences[key] === 1) {
+      result = key;
+      break;
+    }
+  }
+
+  return result;
+};
