6508d9288dbba353d501313050133460695205de
Shafi Khan <76864645+SaMs-Hub@users.noreply.github.com>
2024-07-10 19:57:27 +0530

    Update 11.1 ThreeSum(LC15).js

--- Topics/Two Pointers/11.1 ThreeSum(LC15).js
+++ Topics/Two Pointers/11.1 ThreeSum(LC15).js
@@ -1,23 +1,29 @@
 // App1 - Naive( will throw double chars)
+const arr = [-1, 0, 1, 2, -1, -4];
+
 const threeSum = (arr) => {
-  let result = [];
-  let n = arr.length;
+  const result = [];
+  const n = arr.length;
 
   for (let i = 0; i < n; i++) {
     for (let j = i + 1; j < n; j++) {
       for (let k = j + 1; k < n; k++) {
-        let sum = arr[i] + arr[j] + arr[k];
+        const sum = arr[i] + arr[j] + arr[k];
         if (sum === 0) {
-          let arrayValue = [arr[i], arr[j], arr[k]];
-          result.push(arrayValue);
+          result.push([arr[i], arr[j], arr[k]]);
         }
       }
     }
   }
 
-  return result;
+  if (result.length > 1) {
+    return result;
+  } else {
+    return -1;
+  }
 };
 
+console.log(threeSum(arr));
 
 // App2 - Using two pointers
 const threeSum = (arr) => {
