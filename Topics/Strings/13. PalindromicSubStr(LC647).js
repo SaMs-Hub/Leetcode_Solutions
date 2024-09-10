baaa56b33b8947183e85963eada70d749cee85e3
Shafi Khan <76864645+SaMs-Hub@users.noreply.github.com>
2024-09-10 22:53:48 +0530

    Update 13. PalindromicSubStr(LC647).js

--- Topics/Strings/13. PalindromicSubStr(LC647).js
+++ Topics/Strings/13. PalindromicSubStr(LC647).js
@@ -1,26 +1,51 @@
-
-const getCount = (str, left, right) => {
-    let count = 0;
-    while (left > 0 && right <= str.length && str[left] == str[right]){
-        count++;
-        left--;
-        right++;
+// BF 
+const isPalindrome = (str, left, right) => {
+  while (left < right) {
+    if (str[left] !== str[right]) {
+      return false;
     }
-    
-    return count;
-}
+    right -= 1;
+    left += 1;
+  }
 
-const getPalindromes = (str) => {
-    let evenCount = 0;
-    let oddCount = 0;
-    
-    for (let i = 0; i < str.length; i++){
-        evenCount += getCount(str, i, i);
-        oddCount += getCount(str, i, i + 1);
+  return true;
+};
+
+const countSubstrings = (str) => {
+  const n = str.length;
+
+  let currentCount = 0;x
+  for (let i = 0; i < n; i++) {
+    for (let j = i; j < n; j++) {
+      if (isPalindrome(str, i, j)) {
+        currentCount += 1;
+      }
     }
-    
-    return evenCount + oddCount;
-}
+  }
+
+  return currentCount;
+};
 
 
+
+ */const isPalindrome = (str, left, right = str.length, result) => {
+  while (left >= 0 && right <= str.length - 1 && str[left] === str[right]) {
+    result.push(str.slice(left, right + 1));
+    right += 1;
+    left -= 1;
+  }
+};
+
+const countSubstrings = (str) => {
+  const n = str.length;
+
+  let result = [];
+
+  for (let i = 0; i < n; i++) {
+    isPalindrome(str, i, i, result);
+    isPalindrome(str, i, i + 1, result);
+  }
+;
+  return result.length;
+};
 console.log(getPalindromes("aaa"));
