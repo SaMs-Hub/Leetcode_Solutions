06aae51d440cd74090ee5d7ea5832b292233d456
Shafi Khan <76864645+SaMs-Hub@users.noreply.github.com>
2024-09-10 14:37:08 +0530

    Update 3.1 JumpGame(LC55).js

--- Topics/Greedy/3.1 JumpGame(LC55).js
+++ Topics/Greedy/3.1 JumpGame(LC55).js
@@ -1,16 +1,22 @@
 // Greedy
+
 const canJump = (arr) => {
   let reachable = 0;
-  let n = arr.length;
+  const n = arr.length;
 
   for (let i = 0; i < n; i++) {
-    if (i > reachable) return false;
+    if (i > reachable) {
+      return false;
+    }
     reachable = Math.max(reachable, i + arr[i]);
 
-    if (reachable >= n - 1) break;
+    if (reachable >= n - 1) {
+      break;
+    }
   }
 
   return true;
 };
+
 
 https://leetcode.com/problems/jump-game/description/
