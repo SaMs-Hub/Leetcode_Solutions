e400452106a877390c771f62655b89e72a43c273
Shafi Khan <76864645+SaMs-Hub@users.noreply.github.com>
2024-09-10 21:44:13 +0530

    Create 49. Intersection(LC349).js

---
+++ Topics/Arrays/49. Intersection(LC349).js
@@ -0,0 +1,27 @@
+// BF O(n * m)
+
+var intersection = function(arr1, arr2) {
+     const result = [];
+  arr1.forEach((x) => {
+    if (arr2.includes(x) && result.includes(x) === false) {
+      result.push(x);
+    }
+  });
+
+  return result;
+};
+
+
+
+//  O( n + m)
+
+const findIntersection = (arr1, arr2) => {
+  const set1 = new Set(arr1);
+  const set2 = new Set(arr2);
+
+  const result = [...set1].filter((x) => {
+    return set2.has(x);
+  });
+
+  return result;
+};
