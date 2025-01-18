db214d2a53da3c88345829182fd9263d10182611
Shafi Khan <76864645+SaMs-Hub@users.noreply.github.com>
2025-01-18 12:22:44 +0530

    Update 41.1 ProductExcSelf(LC238).js

--- Topics/Arrays/41.1 ProductExcSelf(LC238).js
+++ Topics/Arrays/41.1 ProductExcSelf(LC238).js
@@ -3,6 +3,35 @@
  * @return {number[]}
  */
 
+// BF 
+
+const getProduct = (arr, position) => {
+  let current = 1;
+  for (let i = 0; i < arr.length; i++) {
+    if (position === i) {
+      continue;
+    } else {
+      current *= arr[i];
+    }
+  }
+
+  return parseInt(current);
+};
+
+const productExceptSelf = (arr) => {
+  const result = [];
+  const n = arr.length;
+
+  for (let i = 0; i < n; i++) {
+    result.push(getProduct(arr, i));
+  }
+
+  return result;
+};
+
+
+// O(n)
+
    const productExceptSelf = (arr) => {
     let result = [];
     let [prefix, postfix] = [1, 1];
