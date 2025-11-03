c861da32cf2f3b417a3fbe80f6a4842c1eb2bdcc
Shafi Khan <76864645+SaMs-Hub@users.noreply.github.com>
2025-11-04 00:14:07 +0530

    Update 47. SecondMax.js

--- Topics/Arrays/47. SecondMax.js
+++ Topics/Arrays/47. SecondMax.js
@@ -1,13 +1,12 @@
 // Using sort - O(n log n)
 const arr = [1, 2, 4, 2, 3, 4];
 
-const secondMax = (arr) => {
-  arr.sort((a, b) => {
+const secondLargest = (arr) => {
+  arr = arr.sort((a, b) => {
     return b - a;
   });
 
   const filteredArray = [...new Set(arr)];
-
   const n = filteredArray.length;
 
   if (n < 2) {
