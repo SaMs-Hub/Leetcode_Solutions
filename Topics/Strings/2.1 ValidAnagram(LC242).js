56f0e3b2f0200f9220b18bb0e4b7faef92b35216
Shafi Khan <76864645+SaMs-Hub@users.noreply.github.com>
2026-04-15 01:58:55 +0530

    Update 2.1 ValidAnagram(LC242).js

--- Topics/Strings/2.1 ValidAnagram(LC242).js
+++ Topics/Strings/2.1 ValidAnagram(LC242).js
@@ -10,29 +10,32 @@ const isAnagram = (a, b) => {
   return sortedA === sortedB;
 };
 
-// App 2 Using maps O(n)
-const isAnagram = (s, t) => {
-  // base case
-  if (s.length !== t.length) return false;
+// App 2 Using maps O(n)const isAnagram = (a, b) => {
+  if (a.length !== b.length) {
+    return false;
+  }
 
-  // creating a map to store values( can also use object);
   const myMap = new Map();
 
-  // inserting everything of s into map;
-  for (let x of s) {
-    if (myMap.has(x)) myMap.set(x, myMap.get(x) + 1);
-    else myMap.set(x, 1);
+  for (let x of a) {
+    if (myMap.has(x)) {
+      myMap.set(x, myMap.get(x) + 1);
+    } else {
+      myMap.set(x, 1);
+    }
   }
 
-  // using other word to deal with existing words
-  for (let x of t) {
-    if (!myMap.has(x)) return false;
-    else myMap.set(x, myMap.get(x) - 1);
-    if (myMap.get(x) === 0) myMap.delete(x);
-  }
+  for (let x of b) {
+    if (myMap.has(x) === false) {
+      return false;
+    } else {
+      myMap.set(x, myMap.get(x) - 1);
+    }
 
+    if (myMap.get(x) === 0) {
+      myMap.delete(x);
+    }
+  }
   return myMap.size === 0;
 };
-
-
 https://leetcode.com/problems/valid-anagram/description/
