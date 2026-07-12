f3b64e56c6e1f243ff73b45d3d1951b8a1fda7c4
Shafi Khan <76864645+SaMs-Hub@users.noreply.github.com>
2026-07-12 17:14:16 +0530

    Update 19. GroupAnagrams(LC49).js

--- Topics/Strings/19. GroupAnagrams(LC49).js
+++ Topics/Strings/19. GroupAnagrams(LC49).js
@@ -1,5 +1,34 @@
 const strings = ["eat", "tea", "tan", "ate", "nat", "bat"];
 
+
+const str = ["eat", "tea", "tan", "ate", "nat", "bat"];
+
+const groupAnagrams = (strings) => {
+  const result = [];
+
+  while (strings.length) {
+    const first = strings.shift();
+    const subResult = [first];
+    const sortedWord = first.split("").sort().join("");
+
+    for (let i = 0; i < strings.length; i++) {
+      const newStr = strings[i].split("").sort().join("");
+
+      if (newStr === sortedWord) {
+        subResult.push(strings[i]);
+        strings.splice(i, 1);
+        i--;
+      }
+    }
+    result.push(subResult);
+  }
+
+  return result;
+};
+
+console.log(groupAnagrams(str));
+
+
 const groupAnagrams = (string) => {
   const map = new Map();
 
