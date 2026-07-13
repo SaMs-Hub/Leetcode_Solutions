03529399ff8ef484f6c376d3437e4e4b7c6e94f3
Shafi Khan <76864645+SaMs-Hub@users.noreply.github.com>
2026-07-14 00:44:49 +0530

    Update 16.1 MaxArea(LC11).js

--- Topics/Two Pointers/16.1 MaxArea(LC11).js
+++ Topics/Two Pointers/16.1 MaxArea(LC11).js
@@ -1,21 +1,29 @@
 // App1 TLE
-const getMaxArea = (arr) => {
-    //let allAreas = [];
-    let maxArea = [];
-    let size = arr.length;
+const arr = [1, 8, 6, 2, 5, 4, 8, 3, 7];
 
-    for (let i = 0; i < size; i++) {
-        for (let j = i + 1; j < size; j++) {
-            let area = (j - i) * Math.min(arr[i], arr[j]);
-            // allAreas.push(area);
-            maxArea = Math.max(maxArea, area);
-        }
+const maxArea = (arr) => {
+  let max = -Infinity;
+  const n = arr.length;
+  let result = [];
+
+  for (let i = 0; i < n; i++) {
+    for (let j = i + 1; j < n; j++) {
+      const length = Math.min(arr[i], arr[j]);
+      const breadth = j - i;
+
+      const area = length * breadth;
+      let subArray = arr.slice(i, j + 1);
+
+      if (area > max) {
+        max = area;
+        result = [...subArray];
+      }
     }
+  }
+  return [max, result];
+};
 
-    //return [maxArea, allAreas];
-    return maxArea;
-
-}
+console.log(maxArea(arr));
 
 
 // App2 Two Pointers O(n ** 2)
