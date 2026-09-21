dfdefdb66b5b34b83c59c1245a06c57817844803
Shafi Khan <76864645+SaMs-Hub@users.noreply.github.com>
2026-09-21 18:26:52 +0530

    Update 2.1 MergeIntervals(LC56).js

--- Topics/Intervals/2.1 MergeIntervals(LC56).js
+++ Topics/Intervals/2.1 MergeIntervals(LC56).js
@@ -1,83 +1,23 @@
-// App 1
-const merge = (arr) => {
-  let changed = true;
+var merge  = (arr) => {
+  arr.sort((a, b) => {
+    return a[0] - b[0];
+  });
 
-  while (changed) {
-    changed = false;
-
-    for (let i = 0; i < arr.length; i++) {
-      for (let j = i + 1; j < arr.length; j++) {
-        const first = arr[i];
-        const second = arr[j];
-
-        if (first[0] <= second[1] && second[0] <= first[1]) {
-          first[0] = Math.min(first[0], second[0]);
-          first[1] = Math.max(first[1], second[1]);
-
-          arr.splice(j, 1);
-
-          changed = true;
-          break;
-        }
-      }
-      if (changed) {
-        break;
-      }
-    }
-  }
-
-  return arr;
-};
-
-
-const mergeIntervals = (arr) => {
-  const n = arr.length;
-  let currentCount = 0;
   const result = [];
-
-  arr.sort((a, b) => {
-    return a[0] - b[0];
-  });
-
-  while (currentCount < n - 1) {
-    if (arr[currentCount][1] >= arr[currentCount + 1][0]) {
-      arr[currentCount + 1][0] = arr[currentCount][0];
-      arr[currentCount + 1][1] = Math.max(
-        arr[currentCount][1],
-        arr[currentCount + 1][1]
-      );
-    } else {
-      result.push(arr[currentCount]);
-    }
-
-    currentCount += 1;
-  }
-
-  result.push(arr[currentCount]);
-
-  return result;
-};
-
-const merge = (arr) => {
-  arr.sort((a, b) => {
-    return a[0] - b[0];
-  });
-  const result = [arr[0]];
-  const n = arr.length;
-
-  for (let i = 1; i < n; i++) {
-    const current = arr[i];
+  arr.forEach((x) => {
+    const [start, end] = x;
     const last = result[result.length - 1];
 
-    if (current[0] <= last[1]) {
-      last[1] = Math.max(last[1], current[1]);
+    if (!last || start > last[1]) {
+      result.push([start, end]);
     } else {
-      result.push(current);
+      last[1] = Math.max(last[1], end);
     }
-  }
+  });
 
   return result;
 };
+
 
 
 
