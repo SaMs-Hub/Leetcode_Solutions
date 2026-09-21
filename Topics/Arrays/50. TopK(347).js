bbab478a2843a6a33083956443797d285ce075b7
Shafi Khan <76864645+SaMs-Hub@users.noreply.github.com>
2026-09-21 19:03:26 +0530

    Update 50. TopK(347).js

--- Topics/Arrays/50. TopK(347).js
+++ Topics/Arrays/50. TopK(347).js
@@ -1,65 +1,42 @@
-// create a map
-// loop through items and store their frequencies
-// convert map entries into an array
-// sort items by frequency
-// take the first k items
-// return them
+// count frequency of each number
+// create buckets where index = frequency
+// put each number into its frequency bucket
+// start from highest frequency
+// collect numbers until k
+// return result
 
-const arr = [3, 3, 1, 2, 2, 3];
-const k = 2;
-
-const kFrequentElement = (arr, k) => {
+const topK = (arr, k) => {
+  const result = [];
   const myMap = new Map();
 
-  for (let x of arr) {
+  arr.forEach((x) => {
     if (myMap.has(x)) {
       myMap.set(x, myMap.get(x) + 1);
     } else {
       myMap.set(x, 1);
     }
+  });
+
+  const buckets = Array.from({ length: arr.length + 1 }).fill("");
+
+  for (let [key, value] of myMap) {
+    if (!buckets[value]) {
+      buckets[value] = [];
+    }
+    buckets[value].push(key);
   }
 
-  const newArr = [...myMap.entries()];
-  newArr.sort((a, b) => {
-    return b[1] - a[1];
+  [...buckets].reverse().forEach((x) => {
+    if (x) {
+      x.forEach((value) => {
+        if (result.length < k) {
+          result.push(value);
+        }
+      });
+    }
   });
-
-  return newArr.slice(0, k).map((x) => {
-    return x[0];
-  });
+  return result;
 };
 
-console.log(kFrequentElement(arr, k));
+console.log(topK(arr, 2));
 
-
-
-const topKFrequent = (arr, k) => {
-  const result = [];
-  const myMap = new Map();
-
-  for (let x of arr) {
-    if (!myMap.has(x)) {
-      myMap.set(x, 1);
-    } else {
-      myMap.set(x, myMap.get(x) + 1);
-    }
-  }
-
-  const bucket = Array.from({ length: arr.length + 1 }).map((x) => {
-    return [];
-  });
-
-  for (const [num, frequency] of myMap) {
-    bucket[frequency].push(num);
-  }
-
-  for (let i = bucket.length - 1; i > 0; i--) {
-    for (let x of bucket[i]) {
-      result.push(x);
-
-      if (result.length === k) {
-        return result;
-      }
-    }
-  }
-};
