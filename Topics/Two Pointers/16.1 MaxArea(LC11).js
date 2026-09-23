acdbbe0ae67e0fe8f3d0444f777c08491f366195
Shafi Khan <76864645+SaMs-Hub@users.noreply.github.com>
2026-09-23 15:34:02 +0530

    Update 16.1 MaxArea(LC11).js

--- Topics/Two Pointers/16.1 MaxArea(LC11).js
+++ Topics/Two Pointers/16.1 MaxArea(LC11).js
@@ -1,52 +1,24 @@
-// App1 TLE
-const arr = [1, 8, 6, 2, 5, 4, 8, 3, 7];
+// Start two pointers at the beginning and end of the array.
+// Calculate the water using width × smaller height.
+// Store the maximum water found so far.
+// Move the pointer with the smaller height inward.
+// If both heights are equal, move either pointer.
+// Keep doing this until the pointers meet.
+// Return the maximum water.
+
+
 
 const maxArea = (arr) => {
-  let max = -Infinity;
-  const n = arr.length;
-  let result = [];
-
-  for (let i = 0; i < n; i++) {
-    for (let j = i + 1; j < n; j++) {
-      const length = Math.min(arr[i], arr[j]);
-      const breadth = j - i;
-
-      const area = length * breadth;
-      let subArray = arr.slice(i, j + 1);
-
-      if (area > max) {
-        max = area;
-        result = [...subArray];
-      }
-    }
-  }
-  return [max, result];
-};
-
-console.log(maxArea(arr));
-
-
-// App2 Two Pointers O(n ** 2)
-const arr = [1, 8, 6, 2, 5, 4, 8, 3, 7];
-
-const maxArea = (arr) => {
-  let max = -Infinity;
-  let result = [];
-  const n = arr.length;
-
   let left = 0;
-  let right = n - 1;
+  let right = arr.length - 1;
+  let maxWater = 0;
 
   while (left < right) {
     const width = right - left;
     const height = Math.min(arr[left], arr[right]);
 
-    const area = width * height;
-
-    if (area > max) {
-      max = area;
-      result = arr.slice(left, right + 1);
-    }
+    const currentWater = width * height;
+    maxWater = Math.max(currentWater, maxWater);
 
     if (arr[left] < arr[right]) {
       left += 1;
@@ -55,9 +27,5 @@ const maxArea = (arr) => {
     }
   }
 
-  return [max, result];
+  return maxWater;
 };
-
-console.log(maxArea(arr));
-
-// https://leetcode.com/problems/container-with-most-water/
